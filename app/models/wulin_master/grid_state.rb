6143d5cb30edc9980669b943938ecae6ea747529
Xuhao <xuhao@rubyfans.com>
2011-09-19 18:32:27 +0800

    Fix bug in app/models/wulin_master/grid_state.rb of wulin_master

--- app/models/wulin_master/grid_state.rb
+++ app/models/wulin_master/grid_state.rb
@@ -5,7 +5,7 @@ module WulinMaster
     if defined? WulinAuth
       belongs_to :user, :class_name => "WulinAuth::User"
     else
-      raise 'WulinAuth is missing! wulin_master gem is dependence on wulin_auth; Put 'gem wulin_auth' to your Gemfile and run "bundle install --path bundle" command.'
+      raise 'WulinAuth is missing! wulin_master gem is dependence on wulin_auth; Put "gem wulin_auth" to your Gemfile and run "bundle install --path bundle" command.'
     end
 
     def self.update_or_create(attrs)
@@ -23,5 +23,5 @@ end
 if defined? WulinAuth
   WulinAuth::User.send(:has_many, :grid_states, :class_name => "WulinMaster::GridState")
 else
-  raise 'WulinAuth is missing! wulin_master gem is dependence on wulin_auth; Put 'gem wulin_auth' to your Gemfile and run "bundle install --path bundle" command.'
+  raise 'WulinAuth is missing! wulin_master gem is dependence on wulin_auth; Put "gem wulin_auth" to your Gemfile and run "bundle install --path bundle" command.'
 end
