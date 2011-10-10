d8a197379d37d26434e2580d1395d40e70caa0e9
Xuhao <xuhao@rubyfans.com>
2011-10-10 18:35:29 +0800

    Hand GridState model when WulinAuth is not defined

--- app/models/wulin_master/grid_state.rb
+++ app/models/wulin_master/grid_state.rb
@@ -1,27 +1,24 @@
-module WulinMaster 
-  class GridState < ::ActiveRecord::Base
-    default_scope :order => 'created_at DESC'
+if defined? WulinAuth
+  module WulinMaster 
+    class GridState < ::ActiveRecord::Base
+      default_scope :order => 'created_at DESC'
 
-    if defined? WulinAuth
       belongs_to :user, :class_name => "WulinAuth::User"
-    else
-      raise 'WulinAuth is missing! wulin_master gem is dependence on wulin_auth; Put "gem wulin_auth" to your Gemfile and run "bundle install --path bundle" command.'
-    end
 
-    def self.update_or_create(attrs)
-      attrs_dup = attrs.dup
-      state_value = attrs_dup.delete(:state_value)
-      if state = where(attrs_dup).first
-        state.update_attribute(:state_value, state_value)
-      else
-        create(attrs)
+      def self.update_or_create(attrs)
+        attrs_dup = attrs.dup
+        state_value = attrs_dup.delete(:state_value)
+        if state = where(attrs_dup).first
+          state.update_attribute(:state_value, state_value)
+        else
+          create(attrs)
+        end
       end
+    
     end
   end
+  
+  WulinAuth::User.send(:has_many, :grid_states, :class_name => "WulinMaster::GridState")
 end
 
-if defined? WulinAuth
-  WulinAuth::User.send(:has_many, :grid_states, :class_name => "WulinMaster::GridState")
-else
-  raise 'WulinAuth is missing! wulin_master gem is dependence on wulin_auth; Put "gem wulin_auth" to your Gemfile and run "bundle install --path bundle" command.'
-end
+# WulinAuth::User.send(:has_many, :grid_states, :class_name => "WulinMaster::GridState") if defined? WulinAuth
