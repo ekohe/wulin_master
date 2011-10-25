b3c00c16dccbdf82ff39baabd700323ff8660457
Maxime Guilbot <maxime@ekohe.com>
2011-10-25 18:53:12 +0800

    Configure wulin_master to precompile master.js and master.css

--- lib/wulin_master/engine.rb
+++ lib/wulin_master/engine.rb
@@ -4,5 +4,8 @@ module WulinMaster
 
   class Engine < Rails::Engine
     engine_name :wulin_master 
+    initializer "add assets to precompile" do |app|
+       app.config.assets.precompile += %w( master/master.js master.css )
+    end
   end
 end
