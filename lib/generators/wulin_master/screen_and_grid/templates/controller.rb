d9df1e4bdc607845d1252ce9cf17fb878ff5b310
Xuhao <xuhao@rubyfans.com>
2011-10-31 12:52:12 +0800

    MISC 15m - Improve the before_grid_render callback method

--- lib/generators/wulin_master/screen_and_grid/templates/controller.rb
+++ lib/generators/wulin_master/screen_and_grid/templates/controller.rb
@@ -1,4 +1,14 @@
 class <%= class_name.pluralize %>Controller < WulinMaster::ScreenController
   controller_for_screen <%= class_name %>Screen
   controller_for_grid :<%= underscored_name %>
+  
+  # You can add a callback when grid is render
+  # 
+  # ==== Args
+  # * +grid+ - Current grid.
+  # * +params+ - Parameters in the current action.
+  # def before_render_grid(grid, params)
+  #   codes........
+  # end
+  
 end
