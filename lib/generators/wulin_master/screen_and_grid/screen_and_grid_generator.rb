9e1baf3c253c888601c3f9bda61d717bea257f82
Xuhao <xuhao@rubyfans.com>
2011-11-08 17:16:50 +0800

    MISC 30m - Add some control option for 'wulin_master:screen_and_grid' generator

--- lib/generators/wulin_master/screen_and_grid/screen_and_grid_generator.rb
+++ lib/generators/wulin_master/screen_and_grid/screen_and_grid_generator.rb
@@ -6,14 +6,19 @@ module WulinMaster
     source_root File.expand_path('../templates', __FILE__)
     #argument :columns, :type => :array, :default => [], :banner => "column column"
     argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
-    class_option :model, :type => :boolean, :default => true, :description => "Create model"
-    class_option :indexes, :type => :boolean, :default => true, :desc => "Add indexes for references and belongs_to columns"
+    class_option :model, :type => :boolean, :default => true, :desc=> "Create model"
+    class_option :migration,  :type => :boolean, :default => true, :desc => 'Create migration file for current grid'
+    class_option :controller,  :type => :boolean, :default => true, :desc => 'Create controller for current grid'
+    class_option :routes,  :type => :boolean, :default => true, :desc => 'Config routes for current grid'
+    class_option :views,  :type => :boolean, :default => true, :desc => 'Config views directory for current grid'
 
     def create_migration_file
+      return unless options[:migration]
       migration_template "grid_migration.rb", "db/migrate/create_#{table_name}.rb"
     end
 
     def create_controller
+      return unless options[:controller]
       template "controller.rb", "app/#{WulinMaster.config.asset_folder_name}/controllers/#{table_name}_controller.rb"
     end
 
@@ -26,14 +31,17 @@ module WulinMaster
     end
 
     def create_model
+      return unless options[:model]
       template "model.rb", "app/#{WulinMaster.config.asset_folder_name}/models/#{underscored_name}.rb" if options.model?
     end
 
     def add_route
+      return unless options[:routes]
       route "resources :#{table_name}"
     end
     
     def create_view_directory
+      return unless options[:views]
       empty_directory "app/#{WulinMaster.config.asset_folder_name}/views/#{table_name}"
     end
     
