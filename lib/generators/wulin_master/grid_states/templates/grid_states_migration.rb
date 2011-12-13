ae1568a91146bfbfa607ea57ed95794ba8ae8cb6
Xuhao <xuhao@rubyfans.com>
2011-12-13 18:12:24 +0800

    MISC - Add exists verify for grid states migrations

--- lib/generators/wulin_master/grid_states/templates/grid_states_migration.rb
+++ lib/generators/wulin_master/grid_states/templates/grid_states_migration.rb
@@ -1,12 +1,14 @@
 class CreateGridStates < ActiveRecord::Migration
   def change
-    create_table :grid_states do |t|
-      t.integer :user_id
-      t.string  :grid_name
-      t.string  :state_type
-      t.text    :state_value
-      t.timestamps
+    unless table_exists?(:grid_states)
+      create_table :grid_states do |t|
+        t.integer :user_id
+        t.string  :grid_name
+        t.string  :state_type
+        t.text    :state_value
+        t.timestamps
+      end
+      add_index :grid_states, :user_id
     end
-    add_index :grid_states, :user_id
   end
 end
