bd3d8211c09a08a77328bbb6fc6a33ecafb57270
Xuhao <xuhao@rubyfans.com>
2012-01-29 16:50:14 +0800

    MISC - type mistake correct in 'action.rb'

--- lib/wulin_master/actions.rb
+++ lib/wulin_master/actions.rb
@@ -92,7 +92,7 @@ module WulinMaster
         @records.each {|record| record.destroy }
         # end
         message = {:success => true }
-      rescue e
+      rescue => e
         message = {:success => false, :error_message => e.message}
       end
       respond_to do |format|
