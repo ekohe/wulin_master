ce8ba7b4814885436b762e82dcd6f0bc285f46ab
Xuhao <xuhao@rubyfans.com>
2012-03-15 18:21:32 +0800

    FEATURE #8813 - Remove paginator on all grids - just keep total count of records "Showing x records". No need to have the page switches and the per page selector anymore.

--- app/assets/javascripts/jquery_plugins/SlickGrid/controls/slick.pager.js
+++ app/assets/javascripts/jquery_plugins/SlickGrid/controls/slick.pager.js
@@ -65,54 +65,54 @@
         {
             $container.empty();
 
-            var $nav = $("<span class='slick-pager-nav' />").appendTo($container);
-            var $settings = $("<span class='slick-pager-settings' />").appendTo($container);
+            // var $nav = $("<span class='slick-pager-nav' />").appendTo($container);
+            // var $settings = $("<span class='slick-pager-settings' />").appendTo($container);
             $status = $("<span class='slick-pager-status' />").appendTo($container);
 
-            $settings
-                    .append("<span class='slick-pager-settings-expanded' style='display:none'>Show: <a data=0>All</a><a data='-1'>Auto</a><a data=25>25</a><a data=50>50</a><a data=100>100</a></span>");
+            // $settings
+                    // .append("<span class='slick-pager-settings-expanded' style='display:none'>Show: <a data=0>All</a><a data='-1'>Auto</a><a data=25>25</a><a data=50>50</a><a data=100>100</a></span>");
 
-            $settings.find("a[data]").click(function(e) {
-                var pagesize = $(e.target).attr("data");
-                if (pagesize != undefined)
-                {
-                    if (pagesize == -1)
-                    {
-                        var vp = grid.getViewport();
-                        setPageSize(vp.bottom-vp.top);
-                    }
-                    else
-                        setPageSize(parseInt(pagesize));
-                }
-            });
+            // $settings.find("a[data]").click(function(e) {
+            //     var pagesize = $(e.target).attr("data");
+            //     if (pagesize != undefined)
+            //     {
+            //         if (pagesize == -1)
+            //         {
+            //             var vp = grid.getViewport();
+            //             setPageSize(vp.bottom-vp.top);
+            //         }
+            //         else
+            //             setPageSize(parseInt(pagesize));
+            //     }
+            // });
 
-            var icon_prefix = "<span class='ui-state-default ui-corner-all ui-icon-container'><span class='ui-icon ";
-            var icon_suffix = "' /></span>";
+            // var icon_prefix = "<span class='ui-state-default ui-corner-all ui-icon-container'><span class='ui-icon ";
+            // var icon_suffix = "' /></span>";
 
-            $(icon_prefix + "ui-icon-lightbulb" + icon_suffix)
-                    .click(function() { $(".slick-pager-settings-expanded").toggle() })
-                    .appendTo($settings);
+            // $(icon_prefix + "ui-icon-lightbulb" + icon_suffix)
+            //         .click(function() { $(".slick-pager-settings-expanded").toggle() })
+            //         .appendTo($settings);
 
-            $(icon_prefix + "ui-icon-seek-first" + icon_suffix)
-                    .click(gotoFirst)
-                    .appendTo($nav);
-
-            $(icon_prefix + "ui-icon-seek-prev" + icon_suffix)
-                    .click(gotoPrev)
-                    .appendTo($nav);
-
-            $(icon_prefix + "ui-icon-seek-next" + icon_suffix)
-                    .click(gotoNext)
-                    .appendTo($nav);
-
-            $(icon_prefix + "ui-icon-seek-end" + icon_suffix)
-                    .click(gotoLast)
-                    .appendTo($nav);
-
-            $container.find(".ui-icon-container")
-                    .hover(function() {
-                        $(this).toggleClass("ui-state-hover");
-                    });
+            // $(icon_prefix + "ui-icon-seek-first" + icon_suffix)
+            //         .click(gotoFirst)
+            //         .appendTo($nav);
+            // 
+            // $(icon_prefix + "ui-icon-seek-prev" + icon_suffix)
+            //         .click(gotoPrev)
+            //         .appendTo($nav);
+            // 
+            // $(icon_prefix + "ui-icon-seek-next" + icon_suffix)
+            //         .click(gotoNext)
+            //         .appendTo($nav);
+            // 
+            // $(icon_prefix + "ui-icon-seek-end" + icon_suffix)
+            //         .click(gotoLast)
+            //         .appendTo($nav);
+            // 
+            // $container.find(".ui-icon-container")
+            //         .hover(function() {
+            //             $(this).toggleClass("ui-state-hover");
+            //         });
 
             $container.children().wrapAll("<div class='slick-pager' />");
         }
@@ -122,11 +122,11 @@
         {
             var state = getNavState();
 
-            $container.find(".slick-pager-nav span").removeClass("ui-state-disabled");
-            if (!state.canGotoFirst) $container.find(".ui-icon-seek-first").addClass("ui-state-disabled");
-            if (!state.canGotoLast) $container.find(".ui-icon-seek-end").addClass("ui-state-disabled");
-            if (!state.canGotoNext) $container.find(".ui-icon-seek-next").addClass("ui-state-disabled");
-            if (!state.canGotoPrev) $container.find(".ui-icon-seek-prev").addClass("ui-state-disabled");
+            // $container.find(".slick-pager-nav span").removeClass("ui-state-disabled");
+            // if (!state.canGotoFirst) $container.find(".ui-icon-seek-first").addClass("ui-state-disabled");
+            // if (!state.canGotoLast) $container.find(".ui-icon-seek-end").addClass("ui-state-disabled");
+            // if (!state.canGotoNext) $container.find(".ui-icon-seek-next").addClass("ui-state-disabled");
+            // if (!state.canGotoPrev) $container.find(".ui-icon-seek-prev").addClass("ui-state-disabled");
 
 
             if (pagingInfo.pageSize == 0)
