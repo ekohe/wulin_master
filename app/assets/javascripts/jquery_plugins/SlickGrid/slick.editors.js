566fbb8c7f89333f53ea8ab61916718079ef7069
Jimmy Huang <pake007@gmail.com>
2012-05-19 15:16:24 +0800

    MISC - add TimeCellEditor editor

--- app/assets/javascripts/jquery_plugins/SlickGrid/slick.editors.js
+++ app/assets/javascripts/jquery_plugins/SlickGrid/slick.editors.js
@@ -421,6 +421,96 @@
             this.init();
         },
 
+        // Time editor, only
+        TimeCellEditor : function(args) {
+            var $input;
+            var defaultValue;
+            var scope = this;
+            var calendarOpen = false;
+
+            this.init = function() {
+                $input = $("<INPUT type=text class='editor-text' />");
+                $input.appendTo(args.container);
+                $input.focus().select();
+                $input.width($input.width() - 18);
+            };
+
+            this.destroy = function() {
+                $.datepicker.dpDiv.stop(true,true);
+                $input.datetimepicker("hide");
+                $input.datetimepicker("destroy");
+                $input.remove();
+            };
+
+            this.show = function() {
+                if (calendarOpen) {
+                    $.datepicker.dpDiv.stop(true,true).show();
+                }
+            };
+
+            this.hide = function() {
+                if (calendarOpen) {
+                    $.datepicker.dpDiv.stop(true,true).hide();
+                }
+            };
+
+            this.position = function(position) {
+                if (!calendarOpen) return;
+                $.datepicker.dpDiv
+                    .css("top", position.top + 30)
+                    .css("left", position.left);
+            };
+
+            this.focus = function() {
+                $input.focus();
+            };
+
+            this.loadValue = function(item) {
+                defaultValue = item[args.column.field];
+                $input.val(defaultValue);
+                $input[0].defaultValue = defaultValue;
+                $input.select();
+                var attrs = {
+                    showOn: "button",
+                    buttonImageOnly: true,
+                    buttonImage: "/assets/calendar.gif",
+                    beforeShow: function() { calendarOpen = true },
+                    onClose: function() { calendarOpen = false },
+                    timeFormat: 'hh:mm' 
+                };
+                if(item.unit && item.unit.unit == "mn")
+                    $.extend(attrs, {
+                        stepMinute: 5,
+                        minuteGrid: 5
+                    });
+                $input.timepicker(attrs);
+            };
+
+            this.serializeValue = function() {
+                return $input.val();
+            };
+
+            this.applyValue = function(item,state) {
+                item[args.column.field] = state;
+            };
+
+            this.isValueChanged = function() {
+                return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
+            };
+
+            this.validate = function() {
+                return {
+                    valid: true,
+                    msg: null
+                };
+            };
+            
+            this.getCell = function(){
+              return $input.parent();
+            };
+
+            this.init();
+        },
         
         // Date cell editor which can handle "yy-mm-dd" format
         StandardDateCellEditor: function(args) {
