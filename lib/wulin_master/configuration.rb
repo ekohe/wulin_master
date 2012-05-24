185a836adb45924d957f35a74069c762f7fbe573
Xuhao <xuhao@rubyfans.com>
2012-05-24 16:52:51 +0800

    FEATURE #10091 - Add the japan 2012 logo and update application title to 'BSS U-20 WOMEN'S CUP 2012'

--- lib/wulin_master/configuration.rb
+++ lib/wulin_master/configuration.rb
@@ -10,10 +10,11 @@ module WulinMaster
   end
   
   class Configuration
-    attr_accessor :app_title, :asset_folder_name
+    attr_accessor :app_title, :app_title_height, :asset_folder_name
     
     def initialize
       self.app_title = 'Undefined App'
+      self.app_title_height = '42px'
       self.asset_folder_name = 'wulin_master'
     end
   end
