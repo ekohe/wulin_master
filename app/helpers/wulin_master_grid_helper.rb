3c24b191437e12e53a79ced3b71ec08dbd97d94b
Jimmy Huang <pake007@gmail.com>
2012-05-28 18:45:05 +0800

    MISC - add default_hour and default_minute to TimeCell column

--- app/helpers/wulin_master_grid_helper.rb
+++ app/helpers/wulin_master_grid_helper.rb
@@ -40,4 +40,12 @@ module WulinMasterGridHelper
   def time_column?(column)
     'true' if column.options[:editor] == 'TimeCellEditor'
   end
+
+  def default_hour(column)
+    column.options[:default_hour]
+  end
+
+  def default_minute(column)
+    column.options[:default_minute]
+  end
 end
