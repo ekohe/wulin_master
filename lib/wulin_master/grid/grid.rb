8312766f66ddc355d1a315c612d576a1c25d2b91
Jimmy Huang <pake007@gmail.com>
2012-06-08 18:49:36 +0800

    BUG - fixed the grid filter column matching logic, match name first, then match foreign key

--- lib/wulin_master/grid/grid.rb
+++ lib/wulin_master/grid/grid.rb
@@ -138,7 +138,7 @@ module WulinMaster
     end
 
     def apply_filter(query, column_name, filtering_value)
-      column = self.columns.find{|c| c.name.to_s == column_name.to_s or c.foreign_key == column_name.to_s }
+      column = self.columns.find{|c| c.name.to_s == column_name.to_s} || self.columns.find{|c| c.foreign_key == column_name.to_s }
       if column and column.options[:filterable] != false
         column.apply_filter(query, filtering_value)
       else
