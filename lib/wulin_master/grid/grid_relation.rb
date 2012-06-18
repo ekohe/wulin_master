7c74f97cc0c783e40b0392357224d2b5e737ab49
Jimmy Huang <pake007@gmail.com>
2012-06-18 19:25:49 +0800

    FEATURE 10518 1h - Disable the multi-selection for master grid

--- lib/wulin_master/grid/grid_relation.rb
+++ lib/wulin_master/grid/grid_relation.rb
@@ -4,13 +4,16 @@ module WulinMaster
     
     module ClassMethods
       # Set master grid, invoked from grid.apply_custom_config method
-      def master_grid(grid_klass, options={})
+      def master_grid(master_grid_klass, options={})
         if options[:screen]
           detail_model = self.model
-          master_grid = grid_klass.constantize.new({screen: options[:screen], format:'json'})   # format as json to skip the toolbar and styling initialize
+          master_grid = master_grid_klass.constantize.new({screen: options[:screen], format:'json'})   # format as json to skip the toolbar and styling initialize
           through = options[:through] || detail_model.reflections[master_grid.model.to_s.underscore.intern].foreign_key
 
-          # call affiliation behavior
+          # disable the multiSelect for master grid
+          master_grid_klass.constantize.options_pool << {:multiSelect => false, :only => [options[:screen].intern]}
+
+          # call affiliation behavior for detail grid
           behavior :affiliation, master_grid_name: master_grid.name, only: [options[:screen].intern], through: through
         end
       end
