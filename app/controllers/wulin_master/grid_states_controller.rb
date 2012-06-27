70f1db9acc78f6bffd20bd2d2913635c0a5e832d
Jimmy Huang <pake007@gmail.com>
2012-06-27 20:27:45 +0800

    MISC - rewrite grid state controller by using ActionController::Metal

--- app/controllers/wulin_master/grid_states_controller.rb
+++ app/controllers/wulin_master/grid_states_controller.rb
@@ -1,12 +1,12 @@
 module WulinMaster
-  class GridStatesController < ApplicationController
+  class GridStatesController < ::ActionController::Metal
     
     def save
       GridState.update_or_create({ user_id: current_user.id, 
                                    grid_name: params[:grid_name], 
                                    state_type: params[:state_type], 
                                    state_value: params[:state_value].to_json })
-      render :nothing => true
+      self.response_body = "success"
     end
     
   end
