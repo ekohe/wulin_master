d7db2563a8c07d8ea58ef30b4cb9cd92bc5e9991
Jimmy Huang <pake007@gmail.com>
2012-07-30 16:13:40 +0800

    MISC - Improved panel js

--- app/assets/javascripts/master/panel.js
+++ app/assets/javascripts/master/panel.js
@@ -1,5 +1,5 @@
-var adjustPanelButtons = function(){
-  var $panel = $(".panel_container");
+var adjustPanelButtons = function(panelId){
+  var $panel = $("#" + panelId);
   var $btns = $panel.find(".panel_btns");
   if($btns.length == 0) return false;
 
