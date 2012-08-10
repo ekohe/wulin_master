fdb2dc1b3c363d6017d7b41f9b4a1d0b7bbcae40
Xuhao <xuhao@rubyfans.com>
2012-08-10 19:14:54 +0800

    MISC - Fix generator bug

--- lib/generators/wulin_master/screen_and_grid/templates/model.rb
+++ lib/generators/wulin_master/screen_and_grid/templates/model.rb
@@ -1,3 +1,3 @@
 class <%= class_name %> < ActiveRecord::Base
-  attr_accessible <% attributes.select {|attr| attr.reference? }.map{|attr| ":#{attr.name}"}.join(", ") -%>
+  attr_accessible <%= attributes.map{|attr| ":#{attr.name}"}.join(", ") -%>
 end
