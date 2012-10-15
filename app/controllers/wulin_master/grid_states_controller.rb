3a20afe7f2d26f7165dc5ad6dd71a17c12521141
Jimmy Huang <pake007@gmail.com>
2012-10-15 12:19:29 +0800

    FEATURE #6416 @1h - Framework: Allow user to save different user states under names and recall a given user state
    - fixed a missing for grid state save action: create new state

--- app/controllers/wulin_master/grid_states_controller.rb
+++ app/controllers/wulin_master/grid_states_controller.rb
@@ -2,7 +2,12 @@ module WulinMaster
   class GridStatesController < ::ActionController::Metal
     def save
       current_state = GridState.current(current_user.id, params[:grid_name])
-      current_state.state_value = JSON(current_state.state_value).merge(params[:state_type] => params[:state_value]).to_json
+      if current_state
+        current_state.state_value = JSON(current_state.state_value).merge(params[:state_type] => params[:state_value]).to_json
+      else
+        current_state = GridState.new(user_id: current_user.id, grid_name: params[:grid_name], 
+          name: 'default', current: true, state_value: {params[:state_type] => params[:state_value]}.to_json)
+      end
       if current_state.save
         self.response_body = "success"
       else
