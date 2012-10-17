aaf1c655ac315619d1907748aca81c50521effd4
Jimmy Huang <pake007@gmail.com>
2012-10-17 17:51:06 +0800

    MISC - fixed a grid state getter bug

--- lib/wulin_master/components/grid/grid_states.rb
+++ lib/wulin_master/components/grid/grid_states.rb
@@ -6,8 +6,7 @@ module WulinMaster
     def states_for_user(user)
       return "false" unless user
       current_state = GridState.current(user.id, self.name)
-      return "false" unless current_state
-      current_state.state_value.presence || {}.to_json
+      current_state.try(:state_value).presence || {}.to_json
     rescue Exception => e
       Rails.logger.info "Exception thrown while trying to get user states: #{e.inspect}"
       "false"
