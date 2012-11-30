6446fdb426e644552f74668aca66830d71d6d879
Xuhao <xuhao@rubyfans.com>
2012-11-30 12:12:34 +0800

    MISC - GridStatesScreen corrections

--- app/screens/grid_states_screen.rb
+++ app/screens/grid_states_screen.rb
@@ -1,10 +1,19 @@
+begin
+  require 'user_grid'
+rescue LoadError
+end
+
 class GridStatesScreen < WulinMaster::Screen
   title 'Grid States Management'
 
   path '/wulin_master/grid_states'
 
-  # should be loaded first, to cache the all users
-  grid UserGrid, width: '45%', height: '100%', css: 'float: right', multi_select: true if Object.const_defined?(:UserGrid)
-  grid GridStateGrid, width: '45%', height: '100%', title: 'Grid States'
-  panel GridStateUserPanel, width: '10%', height: '100%'
+  if defined?(UserGrid)
+    # should be loaded first, to cache the all users
+    grid UserGrid, width: '45%', height: '100%', css: 'float: right', multi_select: true
+    grid GridStateGrid, width: '45%', height: '100%', title: 'Grid States'
+    panel GridStateUserPanel, width: '10%', height: '100%'
+  else
+    grid GridStateGrid, width: '100%', height: '100%', title: 'Grid States'
+  end
 end
