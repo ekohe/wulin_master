a60cb461254fab6e1b09e33d56af460e63f9930e
Jimmy Huang <pake007@gmail.com>
2012-12-13 18:30:06 +0800

    MISC - format the remotemodel js

--- app/assets/javascripts/jquery_plugins/SlickGrid/slick.remotemodel.js
+++ app/assets/javascripts/jquery_plugins/SlickGrid/slick.remotemodel.js
@@ -1,21 +1,21 @@
 // Remote model with pagination
 
 (function($) {
-	function RemoteModel(path, initialFilters, columns) {
-		// private
-		var loadingSize = 200;
-		var preemptiveLoadingSize = 100;
-		var pageSize = 0;
-		var pageNum = 0;
-		var totalRows = 0;
-		var data = {length:0};
+  function RemoteModel(path, initialFilters, columns) {
+    // private
+    var loadingSize = 200;
+    var preemptiveLoadingSize = 100;
+    var pageSize = 0;
+    var pageNum = 0;
+    var totalRows = 0;
+    var data = {length:0};
     var oldData = {length:0};
-		var sortcol = null;
-		var sortdir = 1;
-		var h_request = null;
-		var req = null; // ajax request
-		var req_page;
-		var params = [];
+    var sortcol = null;
+    var sortdir = 1;
+    var h_request = null;
+    var req = null; // ajax request
+    var req_page;
+    var params = [];
     var pagingOptionsChanged = false;
     var grid;
     var loadingIndicator = null;
@@ -34,8 +34,8 @@
     // Connection manager
     var connectionManager = new ConnectionManager();
     
-		// events
-		var onDataLoading = new Slick.Event();
+    // events
+    var onDataLoading = new Slick.Event();
     var onPagingInfoChanged = new Slick.Event();
     var onDataLoaded = new Slick.Event();
 
@@ -72,22 +72,22 @@
       onDataLoaded.notify();
     }
 
-		function isDataLoaded(from,to) {
-			for (var i=from; i<=to; i++) {
-				if (data[i] == undefined || data[i] == null)
-					return false;
-			}
+    function isDataLoaded(from,to) {
+      for (var i=from; i<=to; i++) {
+        if (data[i] == undefined || data[i] == null)
+          return false;
+      }
 
-			return true;
-		}
+      return true;
+    }
 
 
-		function clear() {
-			for (var key in data) {
-				delete data[key];
-			}
-			data.length = 0;
-		}
+    function clear() {
+      for (var key in data) {
+        delete data[key];
+      }
+      data.length = 0;
+    }
 
     function generateUrl(from, to) {
       // pagination - specific to the grid viewport
@@ -151,25 +151,25 @@
       // 2 cases, whether we are in load-as-scroll mode or the per page mode
       if (pageSize==0) {
         // Load as we scroll
-  			if (from < 0)
-  				from = 0;
+        if (from < 0)
+          from = 0;
 
-  			fromPage = Math.floor(from / loadingSize);
-  			
-  			toPage = Math.floor(to / loadingSize);
+        fromPage = Math.floor(from / loadingSize);
+        
+        toPage = Math.floor(to / loadingSize);
 
         
         // Increment fromPage if the page at fromPage is already loaded until we find an area that hasn't been loaded yet
-  			while (data[fromPage * loadingSize] !== undefined && fromPage < toPage)
-  				fromPage++;
+        while (data[fromPage * loadingSize] !== undefined && fromPage < toPage)
+          fromPage++;
 
         // Decrement toPage if the page at fromPage is already loaded until we find an area that hasn't been loaded yet
-  			while (data[toPage * loadingSize] !== undefined && fromPage < toPage)
-  				toPage--;
+        while (data[toPage * loadingSize] !== undefined && fromPage < toPage)
+          toPage--;
 
-  			if (fromPage > toPage || ((fromPage == toPage) && data[fromPage*loadingSize] !== undefined)) {
+        if (fromPage > toPage || ((fromPage == toPage) && data[fromPage*loadingSize] !== undefined)) {
           return [0,0];
-  			}
+        }
       
         for (var i=fromPage; i<=toPage; i++)
           data[i*loadingSize] = null; // null indicates a 'requested but not available yet'
@@ -181,115 +181,115 @@
           return [0,0];
         }
         pagingOptionsChanged = false;
-  			fromPage = pageNum;
-  			toPage = pageNum;
+        fromPage = pageNum;
+        toPage = pageNum;
 
         // for (var i=fromPage; i<=toPage; i++)
         //   data[i*loadingSize] = null; // null indicates a 'requested but not available yet'
         
-  			return [(pageNum * pageSize), pageSize];
+        return [(pageNum * pageSize), pageSize];
       }
     }
 
-		function ensureData(from,to) {
-		  var urlData = generateUrl(from, to);
+    function ensureData(from,to) {
+      var urlData = generateUrl(from, to);
 
-		  // Nothing to load, just return.
-		  if(urlData == null) { return; }
-		  
+      // Nothing to load, just return.
+      if(urlData == null) { return; }
+      
       var url = urlData[0];      
       var normalLoading = urlData[1];
       
       // Store loading size to provide stats. If pageSize is not zero then we are coming from a pager request.
       loadingIndicator.loadingSize = pageSize == 0 ? loadingSize : pageSize;
       connectionManager.createConnection(grid, url, loadingIndicator, onSuccess, onError);
-		}
+    }
 
 
-		function onError(request, textStatus, errorThrown) {
+    function onError(request, textStatus, errorThrown) {
       //
-		}
+    }
 
-		function onSuccess(resp, textStatus, request) {
-		  var from;
-		  var to;
+    function onSuccess(resp, textStatus, request) {
+      var from;
+      var to;
 
-			if (pageSize==0) {
-			  from = resp.offset;
-			  to = resp.offset + resp.count;
+      if (pageSize==0) {
+        from = resp.offset;
+        to = resp.offset + resp.count;
         data.length = parseInt(resp.total);
-	    } else {
-			  from = 0;
-			  to = parseInt(resp.count);
-	      data.length = to;
-	    }
+      } else {
+        from = 0;
+        to = parseInt(resp.count);
+        data.length = to;
+      }
 
-	    totalRows = parseInt(resp.total);
-			for (var i = 0; i < resp.rows.length; i++) {
-			  var j = parseInt(from)+parseInt(i);
-			  var obj = {};
-			  $.each(columns, function(index, value) {
-			    var item = resp.rows[i][index];
-			    // match the column and the response data (compare column name and response data key)
-			    if(item && typeof(item) == 'object' && !(item instanceof Array)) {
+      totalRows = parseInt(resp.total);
+      for (var i = 0; i < resp.rows.length; i++) {
+        var j = parseInt(from)+parseInt(i);
+        var obj = {};
+        $.each(columns, function(index, value) {
+          var item = resp.rows[i][index];
+          // match the column and the response data (compare column name and response data key)
+          if(item && typeof(item) == 'object' && !(item instanceof Array)) {
             $.extend(true, obj, item);
-			    } else {
-			      obj[value.id] = item;    
-			    }     
-			  });
-				data[j] = obj;
-				data[j].slick_index = j;
-			}
-			req = null;
+          } else {
+            obj[value.id] = item;    
+          }     
+        });
+        data[j] = obj;
+        data[j].slick_index = j;
+      }
+      req = null;
       
       // keep oldData as a clone of data, never get deleted
       this.loader.oldData = deep_clone(data);
 
       // Loading data
-			dataIsLoaded({from:from, to:to});
+      dataIsLoaded({from:from, to:to});
 
-			// Updating pager
-			onPagingInfoChanged.notify(getPagingInfo());		
-		}
-		
-		function getColumns() {
-		  return columns;
-		}
-		
-	  function getKeys(h) {
+      // Updating pager
+      onPagingInfoChanged.notify(getPagingInfo());    
+    }
+    
+    function getColumns() {
+      return columns;
+    }
+    
+    function getKeys(h) {
       var keys = new Array();
       for (var key in h)
         keys.push(key);
       return keys;
     } 
 
-		function reloadData(from,to) {
+    function reloadData(from,to) {
       var i;
-		  if (from && to) {
-  			for (i=from; i<=to; i++)
-  				delete data[i];
-		  } else {
-		    for (i=0; i<=data.length; i++)
-  				delete data[i];
-		  }
+      if (from && to) {
+        for (i=from; i<=to; i++)
+          delete data[i];
+      } else {
+        for (i=0; i<=data.length; i++)
+          delete data[i];
+      }
 
-			ensureData(from,to);
-		}
+      ensureData(from,to);
+    }
 
-		function setSort(column,dir) {
-			sortcol = column;
-			sortdir = dir;
-			refresh();
-		}
+    function setSort(column,dir) {
+      sortcol = column;
+      sortdir = dir;
+      refresh();
+    }
 
     function setSortWithoutRefresh(column, dir) {
       sortcol = column;
       sortdir = dir;
     }
-		
-		function getSortColumn() {
-		  return sortcol;
-		}
+    
+    function getSortColumn() {
+      return sortcol;
+    }
 
     function getSortDirection() {
       return sortdir;
@@ -333,10 +333,10 @@
       }
     }
 
-		function addFilter(column, string, operator) {
+    function addFilter(column, string, operator) {
       addFilterWithoutRefresh(column, string, operator);
-			refresh();
-  	}
+      refresh();
+    }
 
     function addFiltersWithoutRefresh(filters) {
       $.each(filters, function(index, filter) {
@@ -349,58 +349,58 @@
       refresh();
     }
 
-  	function getFilters() {
-  	  return filters;
-  	}
+    function getFilters() {
+      return filters;
+    }
 
-		function setParam(column, string, _refresh) {
+    function setParam(column, string, _refresh) {
       if (_refresh === undefined) _refresh = true;
-		  // If the string is an empty string, then removing the param if existing
-			if (string=='') {
-			  var newParams = [];
-			  $.each(params, function(index,param) {
-			    if (param[0]!=column)
-			      newParams.push(param);
-			  });
-			  params = newParams;
-	      if (_refresh) refresh(); // Only clear if it was found
-			  return;
-			}
-						
+      // If the string is an empty string, then removing the param if existing
+      if (string=='') {
+        var newParams = [];
+        $.each(params, function(index,param) {
+          if (param[0]!=column)
+            newParams.push(param);
+        });
+        params = newParams;
+        if (_refresh) refresh(); // Only clear if it was found
+        return;
+      }
+            
       var updated = 0;
       // Try to update existing param
-		  $.map(params, function(param) {
-		    if (param[0]==column) {
-	        param[1] = string;
-	        updated = 1;
-	        return;
-		    }
-		  });
-		  
-		  // Add new param
-		  if (updated==0)
-  		  params.push([column, string]);
-		
-			if (_refresh) refresh();
-  	}
+      $.map(params, function(param) {
+        if (param[0]==column) {
+          param[1] = string;
+          updated = 1;
+          return;
+        }
+      });
+      
+      // Add new param
+      if (updated==0)
+        params.push([column, string]);
+    
+      if (_refresh) refresh();
+    }
 
-  	function getParams() {
-  	  return params;
-  	}
-		
-		function setLoadingIndicator(indicator) {
-		  loadingIndicator = indicator;
-		}
-		
-		function setMainIndicator(indicator) {
-		  mainIndicator = indicator;
-		}
-		
-		function refresh() {
+    function getParams() {
+      return params;
+    }
+    
+    function setLoadingIndicator(indicator) {
+      loadingIndicator = indicator;
+    }
+    
+    function setMainIndicator(indicator) {
+      mainIndicator = indicator;
+    }
+    
+    function refresh() {
       pagingOptionsChanged = true;
-		  clear();
-		  rowsChanged();
-		}
+      clear();
+      rowsChanged();
+    }
 
     function setPagingOptions(args) {
       pagingOptionsChanged = false;
@@ -429,57 +429,57 @@
         pagingOptionsChanged = true;
       }
       
-		  refresh();
-      onPagingInfoChanged.notify(getPagingInfo());      
+      refresh();
+      onPagingInfoChanged.notify(getPagingInfo());
     }
 
     function getPagingInfo() {
       return {pageSize:pageSize, pageNum:pageNum, totalRows:totalRows};
     }
 
-		return {
-			// properties
-			"data": data,
+    return {
+      // properties
+      "data": data,
       "oldData": oldData,
       "connectionManager": connectionManager,
       "lastRequestVersionNumber": lastRequestVersionNumber,
       
-			// methods
-			"clear": clear,
-			"isDataLoaded": isDataLoaded,
-			"ensureData": ensureData,
-			"reloadData": reloadData,
-			"setSort": setSort,
+      // methods
+      "clear": clear,
+      "isDataLoaded": isDataLoaded,
+      "ensureData": ensureData,
+      "reloadData": reloadData,
+      "setSort": setSort,
       "setSortWithoutRefresh": setSortWithoutRefresh,
-			"getSortColumn": getSortColumn,
-			"getSortDirection": getSortDirection,
-			"getFilters": getFilters,
+      "getSortColumn": getSortColumn,
+      "getSortDirection": getSortDirection,
+      "getFilters": getFilters,
       "setFilterWithoutRefresh": setFilterWithoutRefresh,
-			"setFilter": setFilter,
+      "setFilter": setFilter,
       "addFilterWithoutRefresh": addFilterWithoutRefresh,
-			"addFilter": addFilter,
+      "addFilter": addFilter,
       "addFiltersWithoutRefresh": addFiltersWithoutRefresh,
       "addFilters": addFilters,
-			"getParams": getParams,
-			"setParam": setParam,
-			"setGrid": setGrid,
+      "getParams": getParams,
+      "setParam": setParam,
+      "setGrid": setGrid,
       "conditionalURI": conditionalURI,
       'getColumns': getColumns,
       
-			// events
-			"onDataLoading": onDataLoading,
-			"onPagingInfoChanged":  onPagingInfoChanged,
-			"onDataLoaded": onDataLoaded,
-			
-			// pager
-			"getPagingInfo": getPagingInfo,
-			"setPagingOptions": setPagingOptions,
-			
-			"setLoadingIndicator": setLoadingIndicator,
-			"setMainIndicator": setMainIndicator			
-		};
-	}
+      // events
+      "onDataLoading": onDataLoading,
+      "onPagingInfoChanged":  onPagingInfoChanged,
+      "onDataLoaded": onDataLoaded,
+      
+      // pager
+      "getPagingInfo": getPagingInfo,
+      "setPagingOptions": setPagingOptions,
+      
+      "setLoadingIndicator": setLoadingIndicator,
+      "setMainIndicator": setMainIndicator      
+    };
+  }
 
-	// Slick.Data.RemoteModel
-	$.extend(true, window, { Slick: { Data: { RemoteModel: RemoteModel }}});
+  // Slick.Data.RemoteModel
+  $.extend(true, window, { Slick: { Data: { RemoteModel: RemoteModel }}});
 })(jQuery);
