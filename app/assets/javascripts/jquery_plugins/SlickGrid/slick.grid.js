ad8b39c97a7d59ccbaf2a77bf99213bc450679c1
Jimmy Huang <pake007@gmail.com>
2013-01-31 20:15:27 +0800

    BUG - Fix Safari 6 toString() defaultFormatter() bug
    - On Safari 6 in Lion, the toString() method will fail on numeric data if
    the Error Console is not open. By using string concatenation, this gets
    around this strange Safari bug.

--- app/assets/javascripts/jquery_plugins/SlickGrid/slick.grid.js
+++ app/assets/javascripts/jquery_plugins/SlickGrid/slick.grid.js
@@ -1052,15 +1052,10 @@ if (typeof Slick === "undefined") {
         }
 
         function defaultFormatter(row, cell, value, columnDef, dataContext) {
-            console.log(row)
-            console.log(cell)
-            console.log(value)
-            console.log(columnDef)
-            console.log(dataContext)
             if (value == null) {
                 return "";
             } else {
-                return value.toString().replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+                return (value + "").replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
             }
         }
 
