7824d3f3c6a510a58f7e75b7f7a82607e2c73e9b
Jimmy Huang <pake007@gmail.com>
2013-02-20 18:00:56 +0800

    MISC - improve the support of disabled options for chosen, override the functions rather than modifying the source code

--- app/assets/javascripts/overrides/chosen.jquery.js
+++ app/assets/javascripts/overrides/chosen.jquery.js
@@ -7,3 +7,76 @@ Chosen.prototype.search_results_mouseup = function(evt) {
     // return this.search_field.focus();
   }
 };
+
+// ---------------- support disabled options ---------------------------
+AbstractChosen.prototype.result_add_option = function(option) {
+  var classes, style;
+  option.dom_id = this.container_id + "_o_" + option.array_index;
+  classes = option.selected && this.is_multiple ? [] : ["active-result"];
+  if (option.selected) {
+    classes.push("result-selected");
+  }
+  if (option.group_array_index != null) {
+    classes.push("group-option");
+  }
+
+  // --------- support disable -------------
+  if (option.disabled) {
+    classes.push("disabled");
+  }
+  // ---------------------------------------
+
+  if (option.classes !== "") {
+    classes.push(option.classes);
+  }
+  style = option.style.cssText !== "" ? " style=\"" + option.style + "\"" : "";
+  return '<li id="' + option.dom_id + '" class="' + classes.join(' ') + '"' + style + '>' + option.html + '</li>';
+};
+
+Chosen.prototype.result_select = function(evt) {
+  var high, high_id, item, position;
+  if (this.result_highlight) {
+    high = this.result_highlight;
+    high_id = high.attr("id");
+    this.result_clear_highlight();
+    if (this.is_multiple) {
+      this.result_deactivate(high);
+    } else {
+      this.search_results.find(".result-selected").removeClass("result-selected");
+      this.result_single_selected = high;
+      this.selected_item.removeClass("chzn-default");
+    }
+    high.addClass("result-selected");
+    position = high_id.substr(high_id.lastIndexOf("_") + 1);
+    item = this.results_data[position];
+
+    // -------------------- support disabled -------------------
+    if(this.form_field.options[item.options_index].disabled){
+      return false;
+    }
+    //----------------------------------------------------------
+
+    item.selected = true;
+    this.form_field.options[item.options_index].selected = true;
+    if (this.is_multiple) {
+      this.choice_build(item);
+    } else {
+      this.selected_item.find("span").first().text(item.text);
+      if (this.allow_single_deselect) {
+        this.single_deselect_control_build();
+      }
+    }
+    if (!((evt.metaKey || evt.ctrlKey) && this.is_multiple)) {
+      this.results_hide();
+    }
+    this.search_field.val("");
+    if (this.is_multiple || this.form_field_jq.val() !== this.current_value) {
+      this.form_field_jq.trigger("change", {
+        'selected': this.form_field.options[item.options_index].value
+      });
+    }
+    this.current_value = this.form_field_jq.val();
+    return this.search_field_scale();
+  }
+};
+// ---------------- support disabled options ---------------------------
