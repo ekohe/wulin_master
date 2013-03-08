54c7d4817ea732794e6722e6a3ad244348afb691
Xuhao <xuhao@rubyfans.com>
2013-03-08 10:40:35 +0800

    MISC - Override chosen function to fix js error

--- app/assets/javascripts/overrides/chosen.jquery.js
+++ app/assets/javascripts/overrides/chosen.jquery.js
@@ -8,6 +8,23 @@ Chosen.prototype.search_results_mouseup = function(evt) {
   }
 };
 
+Chosen.prototype.search_field_disabled = function() {
+  this.is_disabled = this.form_field_jq[0].disabled;
+  if (this.is_disabled) {
+    this.container.addClass('chzn-disabled');
+    if (this.search_field[0]) this.search_field[0].disabled = true;
+    if (!this.is_multiple) {
+      this.selected_item.unbind("focus", this.activate_action);
+    }
+    return this.close_field();
+  } else {
+    this.container.removeClass('chzn-disabled');
+    if (this.search_field[0]) this.search_field[0].disabled = false;
+    if (!this.is_multiple) {
+      return this.selected_item.bind("focus", this.activate_action);
+    }
+  }
+};
 // ---------------- support disabled options ---------------------------
 AbstractChosen.prototype.result_add_option = function(option) {
   var classes, style;
