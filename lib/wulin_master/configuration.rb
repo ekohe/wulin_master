7151b1cbbe06f2bbd415e0b15e2509b09e12407f
Maxime Guilbot <maxime@ekohe.com>
2013-05-14 13:00:28 +0800

    FEATURE - Remove report_title configuration from wulin master as it is only used by one app

--- lib/wulin_master/configuration.rb
+++ lib/wulin_master/configuration.rb
@@ -10,11 +10,10 @@ module WulinMaster
   end
   
   class Configuration
-    attr_accessor :app_title, :app_title_height, :report_title, :always_reset_form, :default_year
+    attr_accessor :app_title, :app_title_height, :always_reset_form, :default_year
     
     def initialize
       self.app_title = 'Undefined App'
-      self.report_title = 'Undefined Report'
       self.app_title_height = '42px'
       self.always_reset_form = false
       self.default_year = Date.today.year
