bc3ef4df4dacece575708bad6fdb1abf27a21065
Jimmy Huang <pake007@gmail.com>
2013-07-22 17:31:47 +0800

    FEATURE #18383 - Implement grid behavior authorization checking like grid action

--- lib/wulin_master/components/grid/grid_behaviors.rb
+++ lib/wulin_master/components/grid/grid_behaviors.rb
@@ -53,26 +53,45 @@ module WulinMaster
     # ----------------------- Instance Methods ------------------------------
 
     # the behaviors of a grid instance, filtered by screen param from class's behaviors_pool 
-    def behaviors
+    def behaviors(current_user=nil)
       return self.class.behaviors_pool if self.params["screen"].blank?
-      self.class.behaviors_pool.select {|behavior| valid_behavior?(behavior, self.params["screen"])}
+      self.class.behaviors_pool.select {|behavior| valid_behavior?(behavior, self.params["screen"], current_user)}
     end
 
     # return the behavior with options, except the :only or :except option
-    def behavior_configs
-      behaviors.map {|b| b.reject{|k,v| k == :only or k == :except} }
+    def behavior_configs(current_user=nil)
+      the_behaviors = behaviors(current_user)
+      the_behaviors.map {|b| b.reject{|k,v| k == :only or k == :except} }
     end
 
-    def behavior_names
-      behaviors.map {|b| b[:name].to_s}
+    def behavior_names(current_user=nil)
+      the_behaviors = behaviors(current_user)
+      the_behaviors.map {|b| b[:name].to_s}
     end
 
     private
 
-    def valid_behavior?(behavior, screen_name)
+    def valid_behavior?(behavior, screen_name, user)
+      valid_by_screen_configuration?(behavior, screen_name, user) and
+      valid_by_behavior_authorized?(behavior, user)
+    end
+
+    # 1. check if this behavior can be applied in the screen due to :only or :except configuration
+    def valid_by_screen_configuration?(behavior, screen_name, user)
       (behavior[:only].blank? and behavior[:except].blank?) ||
       (behavior[:only].present? and screen_name and behavior[:only].include?(screen_name.intern)) ||
       (behavior[:except].present? and screen_name and behavior[:except].exclude?(screen_name.intern))
+    end
+
+    # 2. check if this behavior authorized for current user
+    def valid_by_behavior_authorized?(behavior, user)
+      return true unless (behavior[:authorized?] and user)
+
+      if behavior[:authorized?].kind_of?(Proc)
+        return behavior[:authorized?].call(user)
+      else
+        return behavior[:authorized?] == true
+      end
     end
     
   end
