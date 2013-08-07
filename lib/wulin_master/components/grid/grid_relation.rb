582d95c1b01dd2c732d710882f49d67c8ca95ac8
Xuhao <xuhao@rubyfans.com>
2013-08-07 18:33:39 +0800

    REFACTOR - grid_relation.rb

--- lib/wulin_master/components/grid/grid_relation.rb
+++ lib/wulin_master/components/grid/grid_relation.rb
@@ -12,35 +12,39 @@ module WulinMaster
     
     module ClassMethods
       # Set master grid, invoked from grid.apply_custom_config method
-      def master_grid(master_grid_klass, options={}, inclusion=true)
+      def master_grid(master_grid_klass_name, options={}, inclusion=true)
         @current_filter_column = nil
 
-        if options[:screen]
-          detail_model = self.model
-          master_grid = master_grid_klass.constantize.new({screen: options[:screen], no_render: true})   # set no_render true to skip the config applying
-          
-          # master_model must has_many detail_model, detail_model may belongs_to master_model OR has_many master_model
-          reflection = detail_model.reflections[master_grid.model.to_s.underscore.intern] || detail_model.reflections[master_grid.model.to_s.underscore.pluralize.intern]
+        if master_grid_klass = master_grid_klass_name.to_s.classify.safe_constantize
+          if options[:screen].present?
+            detail_model = self.model
+            master_grid_name = WulinMaster::Utilities.get_grid_name(master_grid_klass_name, options[:screen])
 
-          through = options[:through] || reflection.foreign_key
+            # master_model must has_many detail_model, detail_model may belongs_to master_model OR has_many master_model
+            reflection = detail_model.reflections[master_grid_klass.model.name.underscore.intern] || detail_model.reflections[master_grid_klass.model.name.underscore.pluralize.intern]
 
-          # call affiliation or reverse_affiliation behavior for detail grid
-          operator = if reflection.macro == :belongs_to
-            inclusion ? 'equals' : 'not_equals'
-          elsif reflection.macro == :has_many
-            inclusion ? 'include' : 'exclude'
+            through = options[:through] || reflection.foreign_key
+
+            # call affiliation or reverse_affiliation behavior for detail grid
+            operator = if reflection.macro == :belongs_to
+              inclusion ? 'equals' : 'not_equals'
+            elsif reflection.macro == :has_many
+              inclusion ? 'include' : 'exclude'
+            end
+
+            # add association column to self for filtering
+            unless self.columns_pool.find {|c| c.full_name == reflection.foreign_key and c.valid_in_screen(options[:screen]) }
+              column reflection.name, visible: false, editable: false, formable: false, option_text_attribute: "id", detail_relation_name: @current_detail_model, only: [options[:screen].intern]
+              @current_filter_column = reflection.name
+            end
+
+            behavior :add_candidate_filter, filter: reflection.foreign_key, only: [options[:screen].intern]
+            behavior :affiliation, master_grid_name: master_grid_name, only: [options[:screen].intern], through: through, operator: operator
+            behavior :empty_detail, master_grid_name: master_grid_name, only: [options[:screen].intern]
+            behavior :clear_detail_when_multi_select, master_grid_name: master_grid_name, only: [options[:screen].intern]
           end
-
-          # add association column to self for filtering
-          unless self.columns_pool.find {|c| c.full_name == reflection.foreign_key and c.valid_in_screen(options[:screen]) }
-            column reflection.name, visible: false, editable: false, formable: false, option_text_attribute: "id", detail_relation_name: @current_detail_model, only: [options[:screen].intern]
-            @current_filter_column = reflection.name
-          end
-
-          behavior :add_candidate_filter, filter: reflection.foreign_key, only: [options[:screen].intern]
-          behavior :affiliation, master_grid_name: master_grid.name, only: [options[:screen].intern], through: through, operator: operator
-          behavior :empty_detail, master_grid_name: master_grid.name, only: [options[:screen].intern]
-          behavior :clear_detail_when_multi_select, master_grid_name: master_grid.name, only: [options[:screen].intern]
+        else
+          raise "'#{master_grid_klass_name}' is not valid grid name."
         end
       end
 
