4bdec0eed4a72425646fb15a049653c9610d0c1b
Xuhao <xuhao@rubyfans.com>
2013-08-09 15:52:50 +0800

    REFACTOR - Changes controllers

--- app/controllers/wulin_master/screen_controller.rb
+++ app/controllers/wulin_master/screen_controller.rb
@@ -1,35 +1,6 @@
 module WulinMaster
-  class ScreenController < ApplicationController
-    include WulinMaster::Actions
 
-    prepend_view_path File.join(File.dirname(__FILE__), '..', '..', 'views')
-    respond_to :html, :json
-    rescue_from ActionView::MissingTemplate, with: :render_index
-
-    helper_method :screen, :components
-
-    class << self
-      attr_accessor :screen_classes, :grid_class, :callbacks
-      
-      def controller_for_screen(*args)
-        self.screen_classes = args
-      end
-
-      def add_callback(name, method_name=nil)
-        @callbacks ||= {}
-        @callbacks[name] ||= []
-        if block_given?
-          @callbacks[name] << lambda { yield }
-        else
-          @callbacks[name] << method_name
-        end
-      end
-
-      def find_callbacks(name)
-        @callbacks[name] 
-      end
-    end
-
+  module Variables
     def screen
       @screen ||= begin
         if params[:screen].present?
@@ -49,30 +20,50 @@ module WulinMaster
         if params[:grid]
           screen.grids.find {|grid| grid.class.name == params[:grid]}
         else
-          screen.grids.find {|grid| grid.model.model_name.plural == controller_name} || screen.grids.first
-        end
+          screen.grids.find {|grid| grid.model.model_name.plural == controller_name}
+        end || screen.grids.first
       end
     end
 
     def components
       @components ||= screen.components
     end
+  end
 
-    private
 
-      def fire_callbacks(name)
-        return unless self.class.callbacks
-        cbs = self.class.find_callbacks(name)
+  class ScreenController < ApplicationController
+    include WulinMaster::Variables
+    include WulinMaster::Actions
 
-        return if cbs.blank?
-        cbs.each do |cb|
-          if cb.class == Proc
-            cb.call
-          else
-            self.send(cb) if self.respond_to?(cb)
-          end
+    prepend_view_path File.join(File.dirname(__FILE__), '..', '..', 'views')
+    respond_to :html, :json
+    rescue_from ActionView::MissingTemplate, with: :render_index
+
+    helper_method :screen, :grid, :components
+
+    class << self
+      attr_accessor :screen_classes, :grid_class, :callbacks
+
+      def controller_for_screen(*args)
+        self.screen_classes = args
+      end
+
+      def add_callback(name, method_name=nil)
+        @callbacks ||= {}
+        @callbacks[name] ||= []
+        if block_given?
+          @callbacks[name] << lambda { yield }
+        else
+          @callbacks[name] << method_name
         end
       end
+
+      def find_callbacks(name)
+        @callbacks[name]
+      end
+    end
+
+    private
 
       def render_index
         render '/index', :layout => (request.xhr? ? false : 'application')
