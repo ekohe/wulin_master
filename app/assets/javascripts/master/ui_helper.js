ekohe/wulin_master

commit e6f54799ecf3ee348b03de30bf4072d9ac936259

fix WulinMaster dynamic drop-down loading issue

app/assets/javascripts/master/ui_helper.js
@@ -128,7 +128,7 @@ var Ui = {
     $('#' + name + '_form input[data-time]').timepicker({});
   },
 
-  setupForm: function(grid, monitor) {
+  setupForm: function(grid, monitor, selectedIndexes) {
     var remotePath = [];
     var choicesColumn = [];
     var distinctColumn = [];
@@ -178,7 +178,7 @@ var Ui = {
               }
             });
             target.append(optionsArr.join(''));
-            Ui.setupChosen(target, monitor);
+            Ui.setupChosen(grid, target, monitor, selectedIndexes);
           });
 
         }
@@ -228,9 +228,13 @@ var Ui = {
             optionsArr.push("<option value='" + value + "'>" + value + "</option>");
           });
           target.append(optionsArr.join(''));
-          Ui.setupChosen(target, monitor);
+          Ui.setupChosen(grid, target, monitor, selectedIndexes);
         }
       });
+    }
+
+    if((distinctColumn.length == remotePath.length == choicesColumn.length == 0) && (typeof(selectedIndexes) != "undefined")){
+      fillValues(scope, grid, selectedIndexes);
     }
 
     first_input = $( '#' + name + '_form input:text', scope).first();
@@ -239,19 +243,20 @@ var Ui = {
     }
   },
 
-  setupChosen: function(dom, monitor) {
-    setTimeout(function(){
-      var afterSetupChosen = dom.data('afterSetupChosen');
-      if (dom.hasClass("chzn-done")) {
-        dom.trigger("liszt:updated");
-      } else {
-        dom.chosen();
-      }
+  setupChosen: function(grid, dom, monitor, selectedIndexes) {
+    if(typeof(selectedIndexes) != "undefined"){
+      fillValues(scope, grid, selectedIndexes);
+    }
+    var afterSetupChosen = dom.data('afterSetupChosen');
+    if (dom.hasClass("chzn-done")) {
+      dom.trigger("liszt:updated");
+    } else {
+      dom.chosen();
+    }
 
-      if( afterSetupChosen ) {
-        afterSetupChosen();
-      }
-    }, 100);
+    if( afterSetupChosen ) {
+      afterSetupChosen();
+    }
   },
 
   // Close dialog

app/assets/javascripts/master/actions/edit.js
@@ -45,7 +45,7 @@ var batchUpdateByAjax = function(grid, version) {
         show: "blind",
         modal: true,
         create: function(event, ui) {
-          Ui.setupForm(grid, true);
+          Ui.setupForm(grid, true, selectedIndexes);
 
           // Check the checkbox when update the file
           checkTheBox(name);
@@ -54,12 +54,6 @@ var batchUpdateByAjax = function(grid, version) {
           submitForm(grid, ids, selectedIndexes);
         },
         open: function(event, ui) {
-
-          // Fill values
-          setTimeout(function(){
-            fillValues(scope, grid, selectedIndexes);
-          }, 1000);
-
           showFlagCheckBox(scope, ids);
         },
         close: function(event, ui) {
@@ -250,7 +244,7 @@ var submitForm = function(grid, ids, selectedIndexes) {
           } else {
             displayNewNotification('1 record updated!');
           }
-          
+
         } else {
           displayErrorMessage(msg.error_message);
           grid.loader.reloadData();

commit e5604408974908898a507f1353b7e4dec44e7d66

BUG - Fix bug introduced in e6f5479 where fill values was not being called if the edit form didn't contain the dynamic columns

app/assets/javascripts/master/ui_helper.js
@@ -155,6 +155,8 @@ var Ui = {
       }
     });
 
+    var fillValuesWillRun = false;
+
     // Fetch select options from remote
     if (remotePath.length > 0) {
       $.each(remotePath, function(i, n) {
@@ -180,7 +182,7 @@ var Ui = {
             target.append(optionsArr.join(''));
             Ui.setupChosen(grid, target, monitor, selectedIndexes);
           });
-
+          fillValuesWillRun = true;
         }
       });
     }
@@ -210,7 +212,7 @@ var Ui = {
             optionsArr.push("<option>Add new Option</option>");
             target.append(optionsArr.join(''));
           });
-
+          fillValuesWillRun = true;
         }
       });
     }
@@ -229,11 +231,12 @@ var Ui = {
           });
           target.append(optionsArr.join(''));
           Ui.setupChosen(grid, target, monitor, selectedIndexes);
+          fillValuesWillRun = true;
         }
       });
     }
 
-    if((distinctColumn.length == remotePath.length == choicesColumn.length == 0) && (typeof(selectedIndexes) != "undefined")){
+    if((fillValuesWillRun==false) && (typeof(selectedIndexes) != "undefined")){
       fillValues(scope, grid, selectedIndexes);
     }
 