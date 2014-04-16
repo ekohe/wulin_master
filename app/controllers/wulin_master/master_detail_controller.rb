f9c6b7bd482f8ba51dd29e2da6895ae0acfdf90f
Maxime Guilbot <maxime@ekohe.com>
2014-04-16 20:21:48 +0800

    FEATURE - Add transaction LOCK in attach_details to prevent duplications and validation rules violations

--- app/controllers/wulin_master/master_detail_controller.rb
+++ app/controllers/wulin_master/master_detail_controller.rb
@@ -9,6 +9,10 @@ module WulinMaster
       middle_model = params[:model].classify.constantize
       detail_column = middle_model.reflections[params[:detail_model].to_sym].foreign_key
       middle_model.transaction do
+
+        # Make sure we are the only one doing this so that validation can actually work properly
+        ActiveRecord::Base.connection.execute("LOCK TABLE #{middle_model.table_name} IN ACCESS EXCLUSIVE MODE;")
+
         params[:detail_ids].each do |detail_id|
           middle_model.create!({detail_column => detail_id, params[:master_column] => params[:master_id]})
         end
