b86b85d37043b0562804592754ec95a151348e06
stephan <stephan@kosmogo.com>
2015-02-27 12:56:23 +0100

    remove min ba-bbq

--- app/assets/javascripts/exception/js_error_reporter.js
+++ app/assets/javascripts/exception/js_error_reporter.js
@@ -1,8 +1,9 @@
 window.onerror = function(msg, file, line){
   var message = "Error: " + msg + "\nurl: " + window.location + "\nfile: " + file + "\nline #: " + line;
-  $.ajax({
-    type: 'POST',
-    url:  'wulin_master/js_error_report',
-    data: {message: message}
-  });
+  alert(message);
+  // $.ajax({
+  //   type: 'POST',
+  //   url:  'wulin_master/js_error_report',
+  //   data: {message: message}
+  // });
 };
