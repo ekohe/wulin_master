3ebf8b919f2374faa18d730440e6783f39eae466
stephan <stephan@kosmogo.com>
2015-03-25 17:56:17 +0100

    master detail work in progress

--- app/assets/javascripts/master/actions/detail_add.js
+++ app/assets/javascripts/master/actions/detail_add.js
@@ -1,36 +1,88 @@
-// Detail grid Toolbar Item 'Add'
-jQuery.event.props.push("cancel");
+// Toolbar Item 'Add Detail' for detail grid
 
-WulinMaster.actions.DetailAdd = $.extend({}, WulinMaster.actions.BaseAction, {
-  name: 'detail_add',
+WulinMaster.actions.AddDetail = $.extend({}, WulinMaster.actions.BaseAction, {
+  name: 'add_detail',
 
   handler: function(e) {
-    var grid = this.getGrid();
-    this.grid = grid;
     var self = this;
-
-    Ui.openDialog(grid, 'wulin_master_new_form', grid.options, function() {setTimeout(function(){
-      self.assignMaster(grid);
-    }, 1000);});
-
-    // register 'Create' button click event, need to remove to dialog action later
-    $('body').off("click", '#' + grid.name + '_submit').on('click', '#' + grid.name + '_submit', function() {
-      Requests.createByAjax(grid, false);
-      return false;
-    });
-    // register 'Create and Continue' button click event, need to remove to dialog action later
-    $('body').off("click", '#' + grid.name + '_submit_continue').on('click', '#' + grid.name + '_submit_continue', function() {
-      Requests.createByAjax(grid, true);
-      return false;
+    var masterId = this.target.master.filter_value;
+    var options = {
+      autoOpen: true,
+      width: 750,
+      height: 600,
+      buttons: {
+        Attach: function() {
+          self.appendNewRecordToMiddleTable(masterId, $modelDialog);
+        },
+        Cancel: function() {
+          $(this).dialog( "destroy" );
+          $modelDialog.remove();
+        }
+      },
+      close: function() {
+        $(this).dialog("destroy");
+        $modelDialog.remove();
+      },
+      open: function(event, ui) {
+        self.getModelGrid(masterId, $modelDialog);
+      }
+    };
+    var $modelDialog = $("<div/>").attr({id: this.model + '_dialog', title: "Attach"}).css('display', 'none').appendTo($('body'));
+    if (this.dialog_options && $.isPlainObject(this.dialog_options)) {
+      $.extend(options, this.dialog_options);
+    }
+    $modelDialog.dialog(options);
+  },
+  
+  getModelGrid: function(masterId, dialogDom) {
+    var master = $.extend({}, this.target.master);
+    var screen = this.screen;
+    // first get controller of the detail model, then open the dialog fill the detail grid
+    $.get('/wulin_master/get_detail_controller?model=' + this.model + '&middle_model=' + this.target.model, function(data){
+      var url = "/" + data.controller + "?screen=" + screen + "&filters[][column]=" + master.filter_column + "&filters[][value]=" + masterId + "&filters[][operator]=exclude";
+      $.ajax({
+        type: 'GET',
+        url: url
+      })
+      .success(function(response){
+        dialogDom.html(response);
+        // copy the target's master to detail grid, just replace the operator to 'exclude'
+        var gridName = dialogDom.find(".grid_container").attr("name");
+        var grid = gridManager.getGrid(gridName);
+        master["filter_operator"] = 'exclude';
+        if(grid.master && grid.master instanceof Array) {
+          grid.master.push($.map(master, function(o){return o;}));
+        } else {
+          grid.master = master;
+        }
+      });
     });
   },
-
-  assignMaster: function(){
-    // TODO
-    // 1. find the master grid and the selected record in master grid
-    // 2. in the create dialog, find the master id dropdown
-    // 3. set the dropdown value as the selected record in master grid, and disable it
+  
+  // Attach
+  appendNewRecordToMiddleTable: function(masterId, dialogDom) {
+    var self = this;
+    var middleModel = this.target.model;
+    var detailGridName = dialogDom.find(".grid_container").attr("name");
+    var detailGrid = gridManager.getGrid(detailGridName);
+    var detailIds = detailGrid.getSelectedIds();
+    
+    if(detailIds.length === 0) {
+      displayErrorMessage("Please select at least one item.");
+    } else {
+      var data = {master_column: this.target.master.filter_column, master_id: masterId, detail_model: this.model, detail_ids: detailIds, model: middleModel};
+      $.post('/wulin_master/attach_master_details', data, function(response){
+        displayNewNotification(response.message);
+        dialogDom.dialog( "destroy" );
+        dialogDom.remove();
+        self.target.loader.reloadData();
+        // reload master grid (in some cases, attaching a detail will affect the master record's data)
+        if(self.reload_master && self.target.master_grid) {
+          self.target.master_grid.loader.reloadData();
+        }
+      });
+    }
   }
 });
 
-WulinMaster.ActionManager.register(WulinMaster.actions.DetailAdd);
+WulinMaster.ActionManager.register(WulinMaster.actions.AddDetail);
--- app/controllers/wulin_master/master_detail_controller.rb
+++ app/controllers/wulin_master/master_detail_controller.rb
@@ -6,8 +6,18 @@ module WulinMaster
     end
 
     def attach_details
-      middle_model = params[:model].classify.constantize
-      detail_column = middle_model.reflections[params[:detail_model].to_sym].foreign_key
+      Rails.logger.info "/\\"*50
+      Rails.logger.info "params = #{params}"
+      Rails.logger.info "/\\"*50
+
+      middle_model = params[:detail_model].classify.constantize
+      Rails.logger.info "passed middle_model = params[:model].classify.constantize #{middle_model}"
+
+      detail_column = middle_model.reflections[params[:model].to_sym].foreign_key
+      Rails.logger.info "passed detail_column = middle_model.reflections[params[:detail_model].to_sym].foreign_key"
+
+      Rails.logger.info "middle_model = #{middle_model} detail_column #{detail_column} "
+      
       middle_model.transaction do
 
         # Make sure we are the only one doing this so that validation can actually work properly
@@ -19,5 +29,32 @@ module WulinMaster
       end
       render :json => {:status => 'OK', :message => "#{self.class.helpers.pluralize(params[:detail_ids].size, 'record')} attached."}
     end
+
+    def attach_master_details
+      Rails.logger.info "/\\"*50
+      Rails.logger.info "params = #{params}"
+      Rails.logger.info "/\\"*50
+
+      middle_model = params[:detail_model].classify.constantize
+      Rails.logger.info "passed middle_model = params[:model].classify.constantize #{middle_model}"
+
+      detail_column = middle_model.reflections[params[:model].to_sym].foreign_key
+      Rails.logger.info "passed detail_column = middle_model.reflections[params[:detail_model].to_sym].foreign_key"
+
+      Rails.logger.info "middle_model = #{middle_model} detail_column #{detail_column} "
+      
+      middle_model.transaction do
+
+        # Make sure we are the only one doing this so that validation can actually work properly
+        ActiveRecord::Base.connection.execute("LOCK TABLE #{middle_model.table_name} IN ACCESS EXCLUSIVE MODE;")
+
+        params[:detail_ids].each do |detail_id|
+          middle_model.create!({detail_column => detail_id, params[:master_column] => params[:master_id]})
+        end
+      end
+      render :json => {:status => 'OK', :message => "#{self.class.helpers.pluralize(params[:detail_ids].size, 'record')} attached."}
+    end
+
+
   end
 end
