36eb49d79c1e515ef87aa5ad87128e3580973d3e
Ben Zheng <ben@ekohe.com>
2016-11-27 17:13:21 -0800

    BUG #60223 - Fix reload feature
    - Add menuable.rb using concerns for controller
    - Inculde Menuable for home_controller & screen_controller
    - Use self.inherited(subclass) to get menu info for subclasseds of home_controller & screen_controller
    - Use xhr? to decide loading content in index template
    - Create content partial to use both in index and home template

--- app/controllers/wulin_master/home_controller.rb
+++ app/controllers/wulin_master/home_controller.rb
@@ -1,5 +1,10 @@
 module WulinMaster
   class HomeController < ApplicationController
+    include WulinMaster::Menuable
+
+    def self.inherited(subclass)
+      subclass.define_menu
+    end
 
     def index
       dashboard if self.respond_to?(:dashboard)
@@ -20,70 +25,6 @@ module WulinMaster
           end
         end
       end
-    end
-
-    class << self
-      attr_accessor :menu_block, :menu, :context
-
-      # Menu definition
-      def menu(&block)
-        @menu_block = block
-      end
-
-      # Called within the block
-      def submenu(title=nil)
-        if block_given?
-          @submenu = SubMenu.new(title)
-          yield
-          @menu << @submenu if @submenu.size > 0
-          @submenu = nil
-        end
-        return @submenu
-      end
-
-      def item(title_or_screen_class, options={})
-        return unless @menu
-        screen_instance = title_or_screen_class.new(context) if title_or_screen_class.kind_of?(Class)
-        authorized_proc = options.delete(:authorized?)
-
-        title = options[:label] ||
-                (title_or_screen_class.respond_to?(:title) ? title_or_screen_class.title : title_or_screen_class.to_s)
-        path = options[:url] ||
-               (screen_instance.respond_to?(:path) ? screen_instance.path : '/')
-
-        if authorized_proc
-          if authorized_proc.kind_of?(Proc)
-            is_authorized = (context && context.respond_to?(:current_user)) ? authorized_proc.call(context.current_user) : authorized_proc.call(nil)
-            return unless is_authorized
-          else
-            return unless (authorized_proc == true)
-          end
-        elsif title_or_screen_class.kind_of?(Class)
-          if screen_instance.respond_to?(:authorized?)
-            is_authorized = (context && context.respond_to?(:current_user)) ? screen_instance.authorized?(context.current_user) : screen_instance.authorized?(nil)
-            return unless is_authorized
-          end
-        end
-        options[:screen_name] = screen_instance.try(:name)
-        if @submenu
-          @submenu << MenuEntry.new(title, path, options)
-        else
-          @menu << MenuEntry.new(title, path, options)
-        end
-      end
-
-      def get_menu
-        @menu
-      end
-    end
-
-    # Generate menu
-    def menu
-      self.class.menu = Menu.new
-      self.class.context = self
-      self.class.menu_block.call(self) unless self.class.menu_block.nil?
-      self.class.context = nil
-      self.class.get_menu
     end
   end
 end
--- app/controllers/wulin_master/screen_controller.rb
+++ app/controllers/wulin_master/screen_controller.rb
@@ -1,6 +1,7 @@
 require 'wulin_master/utilities/variables'
 module WulinMaster
   class ScreenController < ApplicationController
+    include WulinMaster::Menuable
     include WulinMaster::Actions
 
     prepend_view_path File.join(File.dirname(__FILE__), '..', '..', 'views')
@@ -8,6 +9,10 @@ module WulinMaster
     rescue_from ActionView::MissingTemplate, with: :render_index
 
     helper_method :screen, :grid, :components
+
+    def self.inherited(subclass)
+      subclass.define_menu
+    end
 
     class << self
       attr_accessor :screen_classes, :grid_class, :callbacks
@@ -33,8 +38,8 @@ module WulinMaster
 
     private
 
-      def render_index
-        render '/index', :layout => (request.xhr? ? false : 'application')
-      end
+    def render_index
+      render '/index', :layout => (request.xhr? ? false : 'application'), :locals => {xhr: request.xhr?}
+    end
   end
 end
