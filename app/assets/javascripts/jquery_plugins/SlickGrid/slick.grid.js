2431c54595db97a17c0e738fa6ae0755f9a55b0c
Ben Zheng <ben@ekohe.com>
2017-07-10 15:39:10 -0700

    BUG #68628 - Fix the bug that active cell/row resurfaces after column reordering

--- app/assets/javascripts/jquery_plugins/SlickGrid/slick.grid.js
+++ app/assets/javascripts/jquery_plugins/SlickGrid/slick.grid.js
@@ -1369,6 +1369,7 @@ if (typeof Slick === "undefined") {
       } else {
         $('#selection_info_' + self.name).html('');
         $(getActiveCellNode()).removeClass('active');
+        activeCell, activeRow = null;
       }
 
       trigger(self.onSelectedRowsChanged, {rows: getSelectedRows(), grid: self}, e);
