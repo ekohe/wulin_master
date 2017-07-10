494a88eca57d5f6787f57bcb43f2dbf89f17e5d8
Ben Zheng <ben@ekohe.com>
2017-07-10 12:55:10 -0700

    BUG #68627 - Avoid to render loading rows for inline editing in grid

--- app/assets/javascripts/jquery_plugins/SlickGrid/slick.connectionmanager.js
+++ app/assets/javascripts/jquery_plugins/SlickGrid/slick.connectionmanager.js
@@ -12,7 +12,10 @@
       // Ekohe Add: Use progress bar in grid header as indicator
       var $header = grid.container.find('.slick-header');
       var $progress_bar = $('<div class="progress"><div class="indeterminate"></div></div>').appendTo($header);
-      grid.renderLoadingRows({top: 0, bottom: 30});
+      if ($('.slick-row').length == 0) {
+        grid.renderLoadingRows({top: 0, bottom: 30});
+
+      }
       var $loadingRows = grid.container.find('.slick-row.loading');
 
       var newRequest = $.ajax({url: url,
