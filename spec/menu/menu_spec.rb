8536c345c063093006cd7071fad141d3d7650c9a
Ben Zheng <ben@ekohe.com>
2017-10-11 16:29:57 -0700

    MISC #71692 - RSpec works: menu_spec.rb

--- spec/menu/menu_spec.rb
+++ spec/menu/menu_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require './lib/wulin_master/menu/menu'
 
 # Test MenuEntry
 describe WulinMaster::MenuEntry do
-  it "should has title and path" do
-    pending 'fix'
-    @menu_entry = WulinMaster::MenuEntry.new("countries", "/countries")
-    @menu_entry.title.should == "countries"
-    @menu_entry.path.should == "/countries"
+  it 'should has title and path' do
+    @menu_entry = WulinMaster::MenuEntry.new('countries', '/countries', screen_name: 'CountryScreen')
+    expect(@menu_entry.title).to eq('countries')
+    expect(@menu_entry.path).to eq('/countries')
+    expect(@menu_entry.options).to eq(screen_name: 'CountryScreen')
   end
 end
 
@@ -17,41 +19,36 @@ describe WulinMaster::Menu do
     @menu = WulinMaster::Menu.new
   end
 
-  it "should be a kind of array" do
-    @menu.should be_a_kind_of(Array)
+  it 'should be a kind of array' do
+    expect(@menu).to be_a_kind_of(Array)
   end
 
-  it "should render a the right view template" do
-    pending('fix')
-    WulinMaster.stub_chain(:config, :app_title) { "Undefined App" }
-    WulinMaster.stub_chain(:config, :app_title_height) { "40px" }
-    sub_menu_1 = WulinMaster::SubMenu.new("sports")
-    sub_menu_2 = WulinMaster::SubMenu.new("life")
-    @menu << sub_menu_1
-    @menu << sub_menu_2
+  it 'should render the view template' do
+    sub_menu1 = WulinMaster::SubMenu.new('sports')
+    sub_menu2 = WulinMaster::SubMenu.new('life')
+    @menu << sub_menu1
+    @menu << sub_menu2
 
     view = @menu.render
-    view.should match(/Undefined App/)
-    view.should match(/div id=\"navigation\"/)
-    view.should match(/div id=\"activity\"/)
-    view.should match(/div id=\"menu\"/)
-    view.should match(/li class=\"submenu\"/)
-    view.should match(/sports/)
-    view.should match(/life/)
+    expect(view).to match(/div id=\'navigation\'/)
+    expect(view).to match(/div id=\'menu\'/)
+    expect(view).to match(/li class=\'submenu\'/)
+    expect(view).to match(/sports/)
+    expect(view).to match(/life/)
   end
 end
 
 # Test SubMenu
 describe WulinMaster::SubMenu do
   before :each do
-    @sub_menu = WulinMaster::SubMenu.new("sports")
+    @sub_menu = WulinMaster::SubMenu.new('sports')
   end
 
-  it "should be a kind of Menu" do
-    @sub_menu.should be_a_kind_of(WulinMaster::Menu)
+  it 'should be a kind of Menu' do
+    expect(@sub_menu).to be_a_kind_of(WulinMaster::Menu)
   end
 
-  it "should has a title" do
-    @sub_menu.title.should == "sports"
+  it 'should has a title' do
+    expect(@sub_menu.title).to eq('sports')
   end
 end
