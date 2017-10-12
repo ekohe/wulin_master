3f715e83ccfab7c2d6e4f52d3348df60c245f4c1
Ben Zheng <ben@ekohe.com>
2017-10-12 09:49:32 -0700

    REFACTOR - Use freezed contant instead of variable for default_icons in toolbar definition

--- lib/wulin_master/components/grid/toolbar.rb
+++ lib/wulin_master/components/grid/toolbar.rb
@@ -3,30 +3,29 @@
 require 'wulin_master/components/grid/toolbar_item'
 
 module WulinMaster
-  class Toolbar # < Array
+  class Toolbar
     attr_reader :grid_name, :items
 
+    DEFAULT_ICONS = {
+      create: 'add_box',
+      add_detail: 'add_box',
+      export: 'file_download',
+      send_mail: 'mail',
+      edit: 'mode_edit',
+      delete: 'delete',
+      switch: 'launch',
+      audit: 'restore'
+    }.freeze
+
     def initialize(grid_name, actions = [])
-      # self.class.default_items.each {|item| self.unshift(item) }
       @grid_name = grid_name
       @items ||= []
-
-      default_icons = {
-        create: 'add_box',
-        add_detail: 'add_box',
-        export: 'file_download',
-        send_mail: 'mail',
-        edit: 'mode_edit',
-        delete: 'delete',
-        switch: 'launch',
-        audit: 'restore'
-      }
 
       actions.each do |action|
         item_options = {
           id: "#{action[:name]}_action_on_#{grid_name}",
           class: ("#{action[:name]}_action " + action[:class].to_s),
-          icon: (action[:icon] || default_icons[action[:name].to_sym]).to_s,
+          icon: (action[:icon] || DEFAULT_ICONS[action[:name].to_sym]).to_s,
           manually_enable: action[:manually_enable]
         }
         item_options = action.merge(item_options)
