84013bbb8b2d6890b2127e740ef64fcaed990ccd
Ben Zheng <ben@ekohe.com>
2017-10-12 09:13:58 -0700

    MISC #71692 - RSpec works: toolbar_item_spec.rb

--- spec/screen/toolbar_item_spec.rb
+++ spec/screen/toolbar_item_spec.rb
@@ -1,62 +1,56 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 require './lib/wulin_master/components/grid/toolbar_item'
 
 describe WulinMaster::ToolbarItem do
   before :each do
-    @item = WulinMaster::ToolbarItem.new("Excel",
-                                         icon: "excel_icon", javascript: "alert('export excel')", class: "action", id: "excel")
+    @item = WulinMaster::ToolbarItem.new('Excel',
+                                         icon: 'excel_icon',
+                                         javascript: 'alert("export excel")',
+                                         class: 'action',
+                                         id: 'excel')
   end
 
-  it "should has title, javascript, icon, options after initialize" do
-    pending 'fix'
-    @item.title.should == "Excel"
-    @item.javascript.should == "alert('export excel')"
-    @item.icon.should == "excel_icon"
-    @item.options.should == {class: "action", id: "excel", href: "#"}
+  it 'should has title, javascript, icon, options after initialize' do
+    expect(@item.title).to eq('Excel')
+    expect(@item.javascript).to eq('alert("export excel")')
+    expect(@item.icon).to eq('excel_icon')
+    expect(@item.options).to eq(class: 'action', id: 'excel', href: 'javascript:void(0);')
   end
 
-  it "should respond to javascript?" do
-    @item.should respond_to(:javascript?)
-    @item.javascript?.should == true
-
+  it 'should respond to javascript?' do
+    expect(@item).to respond_to(:javascript?)
+    expect(@item.javascript?).to eq(true)
     @item.javascript = nil
-    @item.javascript?.should == false
+    expect(@item.javascript?).to eq(false)
   end
 
-  it "should respond to icon?" do
-    @item.should respond_to(:icon?)
-    @item.icon?.should == true
-
+  it 'should respond to icon?' do
+    expect(@item).to respond_to(:icon?)
+    expect(@item.icon?).to eq(true)
     @item.icon = nil
-    @item.icon?.should == false
+    expect(@item.icon?).to eq(false)
   end
 
-  it "should has anchor_tag_options" do
-    pending 'fix'
-    @item.should respond_to(:anchor_tag_options)
+  it 'should has anchor_tag_options' do
+    expect(@item).to respond_to(:anchor_tag_options)
+
     # when item has both icon and javascript
-    @item.anchor_tag_options.should == {class: "action toolbar_icon_excel_icon", id: "excel", href: "#", onclick: "alert('export excel'); return false;"}
+    expect(@item.anchor_tag_options).to eq(class: 'action', id: 'excel', href: '#', onclick: 'alert("export excel"); return false;')
 
     # when item has only icon
-    @item.stub!(:javascript?) { false }
-    @item.anchor_tag_options.should == {class: "action toolbar_icon_excel_icon", id: "excel"}
+    allow(@item).to receive(:javascript?).and_return(false)
+    expect(@item.anchor_tag_options).to eq(class: 'action', id: 'excel')
 
     # when item has only javascript
-    @item.stub!(:icon?) { false }
-    @item.stub!(:javascript?) { true }
-    @item.anchor_tag_options.should == {class: "action", id: "excel", href: "#", onclick: "alert('export excel'); return false;"}
+    allow(@item).to receive(:icon?).and_return(false)
+    allow(@item).to receive(:javascript?).and_return(true)
+    expect(@item.anchor_tag_options).to eq(class: 'action', id: 'excel', href: '#', onclick: 'alert("export excel"); return false;')
 
     # when item has no icon nor javascript
-    @item.stub!(:icon?) { false }
-    @item.stub!(:javascript?) { false }
-    @item.anchor_tag_options.should == {class: "action", id: "excel"}
-  end
-
-  it "should render the right template" do
-    pending 'fix'
-    view = @item.render
-    view.should include("<div class=\"toolbar_item\">")
-    view.should include("<span>Excel</span>")
-    view.should include("<a class='action toolbar_icon_excel_icon' id='excel' href='#' onclick='alert('export excel'); return false;'")
+    allow(@item).to receive(:icon?).and_return(false)
+    allow(@item).to receive(:javascript?).and_return(false)
+    expect(@item.anchor_tag_options).to eq(class: 'action', id: 'excel')
   end
 end
