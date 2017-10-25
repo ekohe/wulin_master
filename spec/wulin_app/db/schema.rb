c6a47dd1c7f20ef1e726ab1fda778db7a9e87547
Ben Zheng <ben@ekohe.com>
2017-10-24 17:16:08 -0700

    MISC - Add migration of create_count_estimate

--- spec/wulin_app/db/schema.rb
+++ spec/wulin_app/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,26 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301095901) do
+ActiveRecord::Schema.define(version: 20171024160100) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "grid_states", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "grid_name"
-    t.string   "state_value"
-    t.string   "name",        default: "default"
-    t.boolean  "current",     default: false, null: false
+  create_table "grid_states", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "grid_name"
+    t.string "state_value"
+    t.string "name", default: "default"
+    t.boolean "current", default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["user_id"], name: "index_grid_states_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_grid_states_on_user_id"
   end
 
-  create_table "people", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+  create_table "people", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "birthdate"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
