1494bfa45b1e0033c765fd25bbc8b4e9b889108e
Ben Zheng <ben@ekohe.com>
2017-10-25 12:22:00 -0700

    MISC - Add wulin master's db migration path to wulin_app

--- spec/wulin_app/config/environments/test.rb
+++ spec/wulin_app/config/environments/test.rb
@@ -41,4 +41,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.paths['db/migrate'] << '../../db/migrate'
 end
--- spec/wulin_app/db/schema.rb
+++ spec/wulin_app/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171024160100) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,4 +33,5 @@ ActiveRecord::Schema.define(version: 20171024160100) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
