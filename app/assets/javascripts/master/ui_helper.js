9047d42999ad46654e719f36b4f3b7dad65a068b
Ben Zheng <ben@ekohe.com>
2017-11-08 17:36:15 -0800

    BUG #10 - Chosen input CSS adjustment

--- app/assets/javascripts/master/ui_helper.js
+++ app/assets/javascripts/master/ui_helper.js
@@ -236,9 +236,9 @@ var Ui = {
     // Layout
     $('.ui-dialog-titlebar').hide();
     $('.ui-resizable-handle').hide();
-    $('.chzn-container').width('100%');
-    $('.chzn-drop').width('100%');
-    $('.chzn-search input').width('96%');
+    // $('.chzn-container').width('100%');
+    // $('.chzn-drop').width('100%');
+    // $('.chzn-search input').width('96%');
   },
 
   setupChosen: function(dom, monitor) {
