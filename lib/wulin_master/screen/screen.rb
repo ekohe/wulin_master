aeb99d28ef57ec2ff534a537bc19edd025f2695b
Ben Zheng <ben@ekohe.com>
2017-11-17 16:34:14 -0800

    MISC - RoboCop Happy

--- lib/wulin_master/screen/screen.rb
+++ lib/wulin_master/screen/screen.rb
@@ -32,8 +32,8 @@ module WulinMaster
         @grid_configs ||= []
         return unless klass
         @components_pool << klass
-        options.merge!(width: '100%') unless options[:width]
-        options.merge!(height: '100%') unless options[:height]
+        options[:width] ||= '100%'
+        options[:height] ||= '100%'
         @grid_configs << {class: klass}.merge(options)
         options.each do |k, v|
           klass.apply_config(k, v, screen: name)
