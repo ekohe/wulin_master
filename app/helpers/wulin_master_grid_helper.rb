82e645ba3f2b417b2662245d10c6f80e59b7f942
Ben Zheng <ben@ekohe.com>
2017-11-30 16:16:07 -0800

    BUG - Fix the error to adding options to hash type choices

--- app/helpers/wulin_master_grid_helper.rb
+++ app/helpers/wulin_master_grid_helper.rb
@@ -21,14 +21,14 @@ module WulinMasterGridHelper
     elsif choices.is_a?(Hash) # TODO: support hash options
       raw_options = choices.map do |k, v|
         v.inject("<option value=''></option>") do |str, e|
-          str << if e.is_a?(Array)
+          str += if e.is_a?(Array)
                    "<option value='#{e[0]}' data-key='#{k}' style='display:none'>#{e[1]}</option>"
                  else
                    "<option value='#{e}' data-key='#{k}' style='display:none'>#{e}</option>"
                  end
         end
       end
-      raw_options.inject("") { |options, x| options << x }.html_safe
+      raw_options.inject("") { |options, x| options += x }.html_safe
     else
       array_to_options([])
     end
