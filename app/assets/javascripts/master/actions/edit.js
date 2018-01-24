9f637b73abc89d7a8016c5bc21a65740f2a2f0ae
Ben Zheng <ben@ekohe.com>
2018-01-24 14:39:11 -0800

    BUG #29 - Fix the add new options feature for distinct drop down

--- app/assets/javascripts/master/actions/edit.js
+++ app/assets/javascripts/master/actions/edit.js
@@ -156,8 +156,9 @@ var distinctInput = function(inputBox) {
   // This is a crazy feature
   if ($('#' + inputBox.attr('id') + '_chzn li:contains("Add new Option")').size() > 0) {
     addNewSelect = $('#' + inputBox.attr('id'));
-    $('#' + addNewSelect.attr('id') + '_chzn li:contains("Add new Option")').off('mouseup').on('mouseup', function(event) {
+    $('#' + addNewSelect.attr('id') + '_chzn').off('click').on('click', 'li:contains("Add new Option")', function(event) {
       var $select = addNewSelect;
+      $('#' + addNewSelect.attr('id') + '_chzn .chzn-single .search-choice-close').trigger('mouseup');
       var $dialog = $("<div/>").attr({id: 'distinct_dialog', title: "Add new option", 'class': "create_form"}).css('display', 'none').appendTo($('body'));
       var $fieldDiv = $("<div />").attr({style: 'padding: 20px 30px;'});
       var $submitDiv = $("<div />").attr({style: 'padding: 0 30px;'});
