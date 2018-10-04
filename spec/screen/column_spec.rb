320a20c7b66f8f24df73dd340965ff5ef673765f
Maxime Guilbot <maxime@ekohe.com>
2018-10-04 20:21:40 +0900

    MISC - Fix rspec due to column name change

--- spec/screen/column_spec.rb
+++ spec/screen/column_spec.rb
@@ -6,27 +6,30 @@ require './lib/wulin_master/components/grid/column'
 describe WulinMaster::Column do
   before :each do
     @grid = double(:grid)
+    @model = double(:model)
+    allow(@grid).to receive(:model) { @model }
     @column = WulinMaster::Column.new(:title, @grid)
   end
 
-  it 'should has a name' do
+  it 'should have a name' do
     expect(@column.name).to eq(:title)
   end
 
-  it 'should has default options' do
+  it 'should have default options' do
     expect(@column.options).to eq(width: 150, sortable: true)
   end
 
-  it 'can has customized options' do
+  it 'can have customized options' do
     @column = WulinMaster::Column.new(:title, @grid, width: 100, editable: false, label: 'Title')
     expect(@column.options).to eq(width: 100, sortable: true, editable: false, label: 'Title')
   end
 
-  it 'should has a default label' do
-    expect(@column.label).to eq('Title')
+  it 'should have a default label from the localization file' do
+    expect(@model).to receive(:human_attribute_name) { 'Human title' }
+    expect(@column.label).to eq('Human title')
   end
 
-  it 'can has customized label' do
+  it 'can have customized label' do
     @column = WulinMaster::Column.new(:title, @grid, label: 'Post Title')
     expect(@column.label).to eq('Post Title')
   end
