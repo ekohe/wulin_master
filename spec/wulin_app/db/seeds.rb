7082812b9023bf20d16bbd2fb51aae31294eb9cb
Ben Zheng <ben@ekohe.com>
2018-11-10 14:38:45 -0800

    MISC - Add seeds to wulin_app for test

---
+++ spec/wulin_app/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(2000) do |index|
+  Person.create(first_name: sprintf('%04d', index), last_name: 'Test', birthdate: Date.today)
+end
