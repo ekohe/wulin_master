58bf0f841a45ef4e13314f6cf47897a1d013d2d9
feixiong <feixiong@ekohe.com>
2018-12-04 15:26:21 +0800

    MISC #103 - WulinMaster::GridState.create_default should not return nil

--- app/models/wulin_master/grid_state.rb
+++ app/models/wulin_master/grid_state.rb
@@ -41,7 +41,8 @@ module WulinMaster
     end
 
     def self.create_default(user_id, grid_name)
-      return if WulinMaster::GridState.for_user_and_grid(user_id, grid_name).present?
+      grid_srates = WulinMaster::GridState.for_user_and_grid(user_id, grid_name)
+      return grid_srates.first if grid_srates.present?
       create(grid_name: grid_name, user_id: user_id, current: true)
     end
 
