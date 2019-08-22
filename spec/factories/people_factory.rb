860851af226b95df85f38eab25dc134fba1f1057
feixiong <feixiong@ekohe.com>
2019-08-22 12:12:07 +0800

    MISC sonar-next.web#793 - upgrade faker version to 2.1.2

--- spec/factories/people_factory.rb
+++ spec/factories/people_factory.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
   factory :person do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    birthdate { Faker::Date.between(100.years.ago, 16.years.ago) }
+    birthdate { Faker::Date.between(from: 100.years.ago, to: 16.years.ago) }
   end
 end
