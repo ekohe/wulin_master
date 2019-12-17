2e82479fd7c2c3eb1138b2663e9a229668e0dbd2
feixiong <feixiong@ekohe.com>
2019-12-17 16:50:05 +0800

    MISC #72 - trigger a event ‘FormLoaded’ on form shown

--- app/assets/javascripts/master/actions/edit.js
+++ app/assets/javascripts/master/actions/edit.js
@@ -38,6 +38,7 @@ var batchUpdateByAjax = function(grid, version) {
           showFlagCheckBox(modal, ids);
           checkTheBox(name);
           submitForm(grid, ids, selectedIndexes);
+          setTimeout( function() { $(document).trigger('FormLoaded') }, 200);
         }
       });
     });
