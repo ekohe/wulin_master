6c2b2086ae907174c10f8ba742b543b3df2c4eac
Charles Koyeung <charles@ekohe.com>
2019-12-19 17:35:23 +0800

    MISC ekohe/sonar-next.web#904 - Add JS error stack to JavascriptError notification email

--- app/assets/javascripts/exception/js_error_reporter.js
+++ app/assets/javascripts/exception/js_error_reporter.js
@@ -1,8 +1,8 @@
-window.onerror = function(msg, file, line){
+window.onerror = function(msg, file, line, col, error){
   var message = "Error: " + msg + "\nurl: " + window.location + "\nfile: " + file + "\nline #: " + line;
   $.ajax({
     type: 'POST',
     url:  '/wulin_master/js_error_report',
-    data: {message: message}
+    data: { message: message, stack: error.stack }
   });
 };
