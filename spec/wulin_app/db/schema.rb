5f88d6090033d601071c31c6f495baf6e8d6d1d7
Torvalds Du <torvalds@ekohe.com>
2020-04-27 21:37:07 +0800

    MISC #92 - Add vim:boolean job:string to table people

--- spec/wulin_app/db/schema.rb
+++ spec/wulin_app/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_11_103214) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,5 +43,4 @@ ActiveRecord::Schema.define(version: 2020_05_11_103214) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
