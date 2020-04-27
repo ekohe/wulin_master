be73c96b1273b83ef8d2c48287c5ab84410455f8
Torvalds Du <torvalds@ekohe.com>
2020-04-27 21:51:07 +0800

    MISC #92 - Update the db seeds

--- spec/wulin_app/db/seeds.rb
+++ spec/wulin_app/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,6 +8,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+JOBS = %w[Developer DevOps Designer PM].freeze
+
 1.upto(2000) do |index|
-  Person.create(first_name: sprintf('%04d', index), last_name: 'Test', birthdate: Date.today)
+  Person.create(
+    first_name: "first name #{sprintf('%04d', index)}",
+    last_name: "last name #{sprintf('%04d', index)}",
+    job: JOBS.sample,
+    vip: [true, false].sample,
+    birthdate: Date.today
+  )
 end
