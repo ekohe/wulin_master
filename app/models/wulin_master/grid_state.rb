6b005a35a2213601fac2b2a50c8a9188a5ab0613
andrew yang <andrew@ekohe.com>
2020-11-12 15:54:39 +0800

    BUG #171 - Can't create default WulinMasterGridState
    The error happens when try to create default WulinMasterGridState if
    user doesn't exist.
    
    So I add the `optional: true` to skip the validation
    
    I also update Guardfile because it only work in guard v2.But we already
    updated it to v3

--- app/models/wulin_master/grid_state.rb
+++ app/models/wulin_master/grid_state.rb
@@ -90,7 +90,7 @@ module WulinMaster
 end
 
 if WulinMaster::GridState.user_model
-  WulinMaster::GridState.send(:belongs_to, :user, class_name: WulinMaster::GridState.user_model.name)
+  WulinMaster::GridState.send(:belongs_to, :user, class_name: WulinMaster::GridState.user_model.name, optional: true)
   if WulinMaster::GridState.user_model.name == 'WulinAuth::User'
     WulinMaster::GridState.user_model.send(:has_many, :grid_states, class_name: "WulinMaster::GridState")
   end
