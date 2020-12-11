1f7a8ae28faf917721a371e144d3d889bf5ca8a9
Wang <sarahs833>
2020-12-11 15:43:48 +0800

    change the displayErrorMessage jquery text to html

--- app/assets/javascripts/master/dialog.js
+++ app/assets/javascripts/master/dialog.js
@@ -4,8 +4,7 @@ function displayErrorMessage(message, title, width) {
   if (message === undefined) {
     message = "An unexpected error occured.";
   }
-  var message = $('#error-content').text(message);
-  message.html(message.html().replace(/\n/g,'<br/>'));
+  $('#error-content').html(message);
   $('.modal-title').text(title);
   $('#error-modal').modal('open');
 }
