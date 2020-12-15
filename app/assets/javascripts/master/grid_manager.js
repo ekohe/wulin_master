8ea66735e9d9cc4788b42fee4fd07657d1868e68
Wang <sarahs833>
2020-12-15 10:50:01 +0800

    change the obsolete document.execCommand to clipboard api

--- app/assets/javascripts/master/grid_manager.js
+++ app/assets/javascripts/master/grid_manager.js
@@ -338,16 +338,8 @@
       var el = document.createElement('textarea');
       // Set value (string to be copied)
       el.value = str;
-      // Set non-editable to avoid focus and move outside of view
-      el.setAttribute('readonly', '');
-      el.style = {position: 'absolute', left: '-9999px'};
-      document.body.appendChild(el);
-      // Select text inside element
-      el.select();
-      // Copy text to clipboard
-      document.execCommand('copy');
-      // Remove temporary element
-      document.body.removeChild(el);
+      // Copy text to clipoard
+      navigator.clipboard.writeText(el.value);
     }
 
     function setGridBodyHeight(gridElement) {
