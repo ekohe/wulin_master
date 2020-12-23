83df0588248b2b98855b77b66d4046d24c963e5d
andrew yang <andrew@ekohe.com>
2020-12-23 12:45:52 +0800

    MISC wulin_permits#8 - Refactor require_authorization Because the alias method always return true and call authorized method.So I remove the alias method in the method body.

--- lib/wulin_master/authorization.rb
+++ lib/wulin_master/authorization.rb
@@ -7,23 +7,11 @@ module WulinMaster
     # Called as before_action
     def require_authorization
       return true unless respond_to?(:current_user)
-      is_authorized = case params[:action]
-      when 'index'
-        screen.authorized?(current_user)
-      when 'create'
-        screen.authorize_create?(current_user)
-      when 'update'
-        screen.authorize_update?(current_user)
-      when 'destroy'
-        screen.authorize_destroy?(current_user)
-      else
-        true
-      end
 
-      is_authorized ? authorized : unauthorized
+      authorized
     end
 
-    def unauthorized(permission_name=nil)
+    def unauthorized(permission_name = nil)
       Rails.logger.info "Unauthorized #{params[:action].inspect} request to screen #{screen.class}"
       msg = permission_name ? "Permission needed: #{permission_name}" : ''
 
