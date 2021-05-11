dd8e2ade0a8d6c6d7792dc5bd6da78933e870131
Torvalds Du <torvalds@ekohe.com>
2021-05-11 16:02:13 +0800

    MISC - Set wulin_app title

---
+++ spec/wulin_app/config/initializers/wulin_master.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+WulinMaster.configure do |config|
+  # The title of the page, default value is "Undefined App"
+  config.app_title = "Welcome to Wulin World"
+
+  # The height of the application title
+  # config.app_title_height = '40px'
+
+  # For the craete popup form, when click "Create and Continue" button reset the form or not
+  # config.always_reset_form = false
+
+  # The default year
+  # config.default_year = 2017
+
+  # The default color theme for the whole app
+  # config.color_theme = 'blue'
+
+  # The default button mode for the whole app
+  # config.button_mode = 'split'
+
+  # The default nav sidebar partial path for the whole app
+  # config.nav_sidebar_partial_path = ''
+end
