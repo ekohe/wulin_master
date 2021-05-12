90b978471ab23a908959abf65e25f1ed76d1fa25
Torvalds Du <torvalds@ekohe.com>
2021-05-12 15:41:01 +0800

    MISC #218 - Rename variable

--- app/assets/javascripts/master/actions/fullscreen.js
+++ app/assets/javascripts/master/actions/fullscreen.js
@@ -3,8 +3,8 @@ WulinMaster.actions.fullscreen = {
   name: 'fullscreen',
 
   handler: function (e, args) {
-    let $fullscreen = $(e.currentTarget);
-    let fullscreen = $fullscreen.data('fullscreen');
+    let $fullscreenElement = $(e.currentTarget);
+    let fullscreen = $fullscreenElement.data('fullscreen');
     let grid = this.getGrid();
     let currentContainer = grid.container;
     Array.from(currentContainer.siblings(':not(script)')).forEach((sbl) =>
@@ -12,25 +12,27 @@ WulinMaster.actions.fullscreen = {
     );
 
     if (fullscreen) {
-      let gridOriginalHeight = $fullscreen.data('grid_height');
-      let gridOriginalWidth = $fullscreen.data('grid_width');
-      $fullscreen.removeData('fullscreen', 'grid_height', 'grid_width');
+      let gridOriginalHeight = $fullscreenElement.data('grid_height');
+      let gridOriginalWidth = $fullscreenElement.data('grid_width');
+      $fullscreenElement.removeData('fullscreen', 'grid_height', 'grid_width');
       currentContainer.height(gridOriginalHeight).width(gridOriginalWidth);
     } else {
-      $fullscreen.data('fullscreen', true);
+      $fullscreenElement.data('fullscreen', true);
       let gridContainerHeight = currentContainer[0].style.height;
       let gridContainerWidth = currentContainer[0].style.width;
-      $fullscreen.data('grid_height', gridContainerHeight);
-      $fullscreen.data('grid_width', gridContainerWidth);
+      $fullscreenElement.data('grid_height', gridContainerHeight);
+      $fullscreenElement.data('grid_width', gridContainerWidth);
 
       currentContainer.height('100%').width('100%');
     }
     currentContainer.resize();
 
-    let $icon = $fullscreen.find('a.fullscreen_action, i.material-icons');
+    let $icon = $fullscreenElement.find(
+      'a.fullscreen_action, i.material-icons'
+    );
     let $iconText = $icon.text();
     $icon.text($iconText === 'fullscreen' ? 'fullscreen_exit' : 'fullscreen');
-    let $iconLabel = $fullscreen.find('a.fullscreen_action, span');
+    let $iconLabel = $fullscreenElement.find('a.fullscreen_action, span');
     let $iconLabelText = $iconLabel.text();
     $iconLabel.text(
       $iconLabelText === 'Fullscreen' ? 'Fullscreen Exit' : 'Fullscreen'
