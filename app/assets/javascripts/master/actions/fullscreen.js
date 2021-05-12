2852d5662b553efe25ae3f576165e1c01c4f10d1
Torvalds Du <torvalds@ekohe.com>
2021-05-12 17:25:48 +0800

    MISC #218 - Format codes

--- app/assets/javascripts/master/actions/fullscreen.js
+++ app/assets/javascripts/master/actions/fullscreen.js
@@ -1,54 +1,58 @@
 WulinMaster.actions.fullscreen = {
   ...WulinMaster.actions.BaseAction,
   name: 'fullscreen',
-
   handler: function (e, args) {
-    let $fullscreenElement = $(e.currentTarget);
-    let grid = this.getGrid();
-    let currentContainer = grid.container;
+    let $fullscreenElement = $(e.currentTarget)
+    let grid = this.getGrid()
+    let currentContainer = grid.container
 
-    toggleSiblings(currentContainer);
+    toggleSiblings(currentContainer)
 
-    transform($fullscreenElement, currentContainer);
+    transform($fullscreenElement, currentContainer)
 
-    switchIcon($fullscreenElement);
+    switchIcon($fullscreenElement)
   },
-};
+}
 
 const toggleSiblings = (currentContainer) => {
   Array.from(currentContainer.siblings(':not(script)')).forEach((sbl) =>
     $(sbl).toggle()
-  );
-};
-
-const switchIcon = ($target) => {
-  let $icon = $target.find('a.fullscreen_action, i.material-icons');
-  let $iconText = $icon.text();
-  $icon.text($iconText === 'fullscreen' ? 'fullscreen_exit' : 'fullscreen');
-  let $iconLabel = $target.find('a.fullscreen_action, span');
-  let $iconLabelText = $iconLabel.text();
-  $iconLabel.text(
-    $iconLabelText === 'Fullscreen' ? 'Exit Fullscreen' : 'Fullscreen'
-  );
-};
+  )
+}
 
 const transform = ($target, currentContainer) => {
-  let isFullscreen = $target.data('fullscreen');
+  let isFullscreen = $target.data('fullscreen')
   if (isFullscreen) {
-    let gridOriginalHeight = $target.data('grid_height');
-    let gridOriginalWidth = $target.data('grid_width');
-    $target.removeData('fullscreen', 'grid_height', 'grid_width');
-    currentContainer.height(gridOriginalHeight).width(gridOriginalWidth);
+    let gridOriginalHeight = $target.data('grid_height')
+    let gridOriginalWidth = $target.data('grid_width')
+
+    $target.removeData('fullscreen', 'grid_height', 'grid_width')
+
+    currentContainer.height(gridOriginalHeight).width(gridOriginalWidth)
   } else {
-    $target.data('fullscreen', true);
-    let gridContainerHeight = currentContainer[0].style.height;
-    let gridContainerWidth = currentContainer[0].style.width;
-    $target.data('grid_height', gridContainerHeight);
-    $target.data('grid_width', gridContainerWidth);
+    let gridContainerHeight = currentContainer[0].style.height
+    let gridContainerWidth = currentContainer[0].style.width
 
-    currentContainer.height('100%').width('100%');
+    $target.data({
+      fullscreen: true,
+      grid_width: gridContainerWidth,
+      grid_height: gridContainerHeight,
+    })
+
+    currentContainer.height('100%').width('100%')
   }
-  currentContainer.resize();
-};
+  currentContainer.resize()
+}
 
-WulinMaster.ActionManager.register(WulinMaster.actions.fullscreen);
+const switchIcon = ($target) => {
+  let $icon = $target.find('a.fullscreen_action, i.material-icons')
+  let $iconText = $icon.text()
+  $icon.text($iconText === 'fullscreen' ? 'fullscreen_exit' : 'fullscreen')
+  let $iconLabel = $target.find('a.fullscreen_action, span')
+  let $iconLabelText = $iconLabel.text()
+  $iconLabel.text(
+    $iconLabelText === 'Fullscreen' ? 'Exit Fullscreen' : 'Fullscreen'
+  )
+}
+
+WulinMaster.ActionManager.register(WulinMaster.actions.fullscreen)
