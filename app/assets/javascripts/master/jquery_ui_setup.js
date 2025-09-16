e94614cde110936fe4c5785056355e3a080916ff
Maxime Guilbot <maxime@ekohe.com>
2025-09-16 17:12:59 +0900

    Fix bug when creating / updating records

--- app/assets/javascripts/master/jquery_ui_setup.js
+++ app/assets/javascripts/master/jquery_ui_setup.js
@@ -8,6 +8,28 @@ import 'jquery-ui/ui/widgets/sortable'
 
 window.$.ui = ui
 
+// Add keyCode constants that are missing from the jQuery UI import
+if (!$.ui.keyCode) {
+  $.ui.keyCode = {
+    BACKSPACE: 8,
+    COMMA: 188,
+    DELETE: 46,
+    DOWN: 40,
+    END: 35,
+    ENTER: 13,
+    ESCAPE: 27,
+    HOME: 36,
+    LEFT: 37,
+    PAGE_DOWN: 34,
+    PAGE_UP: 33,
+    PERIOD: 190,
+    RIGHT: 39,
+    SPACE: 32,
+    TAB: 9,
+    UP: 38
+  };
+}
+
 // Ensure disableSelection is available on jQuery objects
 if (!$.fn.disableSelection) {
   $.fn.disableSelection = function() {
