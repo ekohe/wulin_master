e116de7c5ada9a17484d83469d2eba71919c4851
Maxime Guilbot <maxime@ekohe.com>
2025-09-18 11:00:58 +0900

    fix specs

--- spec/wulin_app/db/schema.rb
+++ spec/wulin_app/db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_152900) do
+ActiveRecord::Schema[8.0].define(version: 2021_06_15_152900) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "addresses", id: :serial, force: :cascade do |t|
     t.string "country"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 2021_06_15_152900) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "courses", id: :serial, force: :cascade do |t|
